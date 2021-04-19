729d37a81c37b9614229931639d4cb3d03446531
Matt Harvey <mattharvey@google.com>
2021-04-19 17:47:10 +0000

    Merge "Adds cantrip settings for seL4 kernel internal logger and bbl enablement"
    GitOrigin-RevId: 8d5a288654f428138bdd6ba0074aa74327773c86

--- easy-settings.cmake
+++ easy-settings.cmake
@@ -1,5 +1,9 @@
 set(CAMKES_APP "system" CACHE STRING "The one and only CAmkES application in this project")
-set(SIMULATION ON CACHE BOOL "Whether to build simulate script")
-set(RELEASE OFF CACHE BOOL "Performance optimized build")
+
 set(PLATFORM "spike" CACHE STRING "The one and only seL4 platform for Sparrow")
 set(KernelSel4Arch "riscv32" CACHE STRING "Specifies 32-bit branch of the seL4 spike platform")
+
+set(LibUtilsDefaultZfLogLevel 5 CACHE STRING "seL4 internal logging level (0-5).")
+set(SIMULATION ON CACHE BOOL "Whether to build simulate script")
+set(RELEASE OFF CACHE BOOL "Performance optimized build")
+set(UseRiscVBBL ON CACHE BOOL "Whether to use bbl")
