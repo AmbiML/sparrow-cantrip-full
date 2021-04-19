b39d054aadbd0b3603a3a440ed68d8f335760ac7
Matt Harvey <mattharvey@google.com>
2021-04-19 15:00:11 -0700

    Switches platform from spike to sparrow
    Change-Id: Ie086289080bb94e6cf3bc0e28a101d2e0db2000c
    GitOrigin-RevId: 6de1d47766d977281f5cab7671637c31b2220d52

--- easy-settings.cmake
+++ easy-settings.cmake
@@ -1,6 +1,6 @@
 set(CAMKES_APP "system" CACHE STRING "The one and only CAmkES application in this project")
 
-set(PLATFORM "spike" CACHE STRING "The one and only seL4 platform for Sparrow")
+set(PLATFORM "sparrow" CACHE STRING "The one and only seL4 platform for Sparrow")
 set(KernelSel4Arch "riscv32" CACHE STRING "Specifies 32-bit branch of the seL4 spike platform")
 
 set(LibUtilsDefaultZfLogLevel 5 CACHE STRING "seL4 internal logging level (0-5).")
