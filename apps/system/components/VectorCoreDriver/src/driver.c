8196a69a32ac3713e39545b4d999c415df7df56f
Cindy Liu <hcindyl@google.com>
2021-09-15 22:03:31 +0000

    Merge "cantrip:VectorCoreDriver: Use HW register header."
    GitOrigin-RevId: 7b10748eb801311b291aeba25cc0535f4e0b09d4

--- apps/system/components/VectorCoreDriver/src/driver.c
+++ apps/system/components/VectorCoreDriver/src/driver.c
@@ -1,8 +1,9 @@
 #include <camkes.h>
 #include <stdint.h>
 
-// TODO: Set offsets into memory based on `csr`.
-#define CTRL (csr + 0x0)
+#include "vc_top/vc_top.h"
+
+#define CTRL (csr + VC_TOP_CTRL_REG_OFFSET)
 
 void vctop_set_ctrl(uint32_t ctrl) {
     *((volatile uint32_t*)CTRL) = ctrl;
