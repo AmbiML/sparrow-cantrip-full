29312a9dcfd0aa743413d4c921b92caf8b4e8694
Matt Harvey <mattharvey@google.com>
2021-09-14 18:55:26 -0700

    cargo fmt zmodem
    This change does no more than run "cargo fmt" on the sources in the
    zmodem directory so that the porting change can be based on formatted
    files.
    
    Change-Id: I72789f57bf4d195bdcbd24d04cc08e5d801b6b05
    GitOrigin-RevId: 8a27fb27e976c82c10030b6afecfa0fb1980630b

--- apps/system/components/DebugConsole/zmodem/src/bin/rzm.rs
+++ apps/system/components/DebugConsole/zmodem/src/bin/rzm.rs
@@ -1,22 +1,20 @@
 extern crate zmodem;
 
-extern crate log;
-extern crate env_logger;
 extern crate clap;
+extern crate env_logger;
+extern crate log;
 
 mod stdinout;
 
+use clap::{App, Arg};
 use std::fs::File;
 use std::path::Path;
-use clap::{Arg, App};
 
 fn main() {
     env_logger::init().unwrap();
 
     let matches = App::new("Pure Rust implementation of rz utility")
-        .arg(Arg::with_name("file")
-             .required(false)
-             .index(1))
+        .arg(Arg::with_name("file").required(false).index(1))
         .get_matches();
 
     let fileopt = matches.value_of("file").unwrap_or("rz-out");
