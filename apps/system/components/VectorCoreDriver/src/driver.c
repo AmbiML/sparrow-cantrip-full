3117e79ecd499096568f6504493c28ce6b8a956d
Cindy Liu <hcindyl@google.com>
2021-09-17 22:36:44 -0700

    Add VectorCoreDriver interrupt handlers
    Bug: 199798457
    Change-Id: I2899022b9d6d57304d70c50b08e08d225d10c511
    GitOrigin-RevId: 22a051696dac158c43df42ccd6a436e314f25f70

--- apps/system/components/VectorCoreDriver/src/driver.c
+++ apps/system/components/VectorCoreDriver/src/driver.c
@@ -8,6 +8,42 @@
 #define VCTOP_REG(name) \
   *((volatile uint32_t *)(CSR_OFFSET + VC_TOP_##name##_REG_OFFSET))
 
+// CAmkES initialization hook.
+//
+// Enables Interrupts.
+//
+void pre_init() {
+  // Enables interrupts.
+  VCTOP_REG(INTR_ENABLE) = (BIT(VC_TOP_INTR_COMMON_HOST_REQ_BIT) |
+                            BIT(VC_TOP_INTR_ENABLE_FINISH_BIT) |
+                            BIT(VC_TOP_INTR_COMMON_INSTRUCTION_FAULT_BIT) |
+                            BIT(VC_TOP_INTR_COMMON_DATA_FAULT_BIT));
+}
+
 void vctop_set_ctrl(uint32_t ctrl) {
   VCTOP_REG(CTRL) = ctrl;
+}
+
+void host_req_handle(void) {
+  // Also need to clear the INTR_STATE (write-1-to-clear).
+  VCTOP_REG(INTR_STATE) = BIT(VC_TOP_INTR_STATE_HOST_REQ_BIT);
+  seL4_Assert(host_req_acknowledge() == 0);
+}
+
+void finish_handle(void) {
+  // Also need to clear the INTR_STATE (write-1-to-clear).
+  VCTOP_REG(INTR_STATE) = BIT(VC_TOP_INTR_STATE_FINISH_BIT);
+  seL4_Assert(finish_acknowledge() == 0);
+}
+
+void instruction_fault_handle(void) {
+  // Also need to clear the INTR_STATE (write-1-to-clear).
+  VCTOP_REG(INTR_STATE) = BIT(VC_TOP_INTR_STATE_INSTRUCTION_FAULT_BIT);
+  seL4_Assert(instruction_fault_acknowledge() == 0);
+}
+
+void data_fault_handle(void) {
+  // Also need to clear the INTR_STATE (write-1-to-clear).
+  VCTOP_REG(INTR_STATE) = BIT(VC_TOP_INTR_STATE_DATA_FAULT_BIT);
+  seL4_Assert(data_fault_acknowledge() == 0);
 }
