eb150c01bbea349034ab87cd27ca091c95f259c5
Sam Leffler <sleffler@google.com>
2021-12-08 01:52:38 +0000

    Merge "processmanager: add MCS config knob"
    GitOrigin-RevId: 15bb0b52de990f0c556fc577ad0c902750d30ed9

--- easy-settings.cmake
+++ easy-settings.cmake
@@ -2,6 +2,7 @@ set(CAMKES_APP "system" CACHE STRING "The one and only CAmkES application in thi
 
 set(PLATFORM "sparrow" CACHE STRING "The one and only seL4 platform for Sparrow")
 set(KernelSel4Arch "riscv32" CACHE STRING "Specifies 32-bit branch of the seL4 spike platform")
+set(KernelIsMCS OFF CACHE BOOL "Disable seL4 MCS support")
 
 set(LibUtilsDefaultZfLogLevel 5 CACHE STRING "seL4 internal logging level (0-5).")
 set(SIMULATION ON CACHE BOOL "Whether to build simulate script")
