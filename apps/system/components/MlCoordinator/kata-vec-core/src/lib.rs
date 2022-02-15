2e5eeeb06704915c76c85a57b8b0af131caccdd1
Cindy Liu <hcindyl@google.com>
2022-02-15 15:53:04 -0800

    cantrip: vec_core: Update ITCM memory address
    To match HW def in
    https://sparrow.git.corp.google.com/sparrow/3p/lowrisc/opentitan/+/3d05e14abb23e275f68080b59c21e3d949176605/hw/top_matcha/data/top_matcha.hjson#749
    
    Change-Id: I933db00d1e6702fb38544b6641dabcb0d6b6e390
    GitOrigin-RevId: 31de9893d54e43db60d000f03561f22d59482ad4

--- apps/system/components/MlCoordinator/kata-vec-core/src/lib.rs
+++ apps/system/components/MlCoordinator/kata-vec-core/src/lib.rs
@@ -14,7 +14,7 @@ use xmas_elf::ElfFile;
 // TODO(jesionowski): Move these constants to an auto-generated file.
 // TODO(b/214092253): ITCM size blow-up needs to be addressed.
 const ITCM_SIZE: usize = 0x100000;
-const ITCM_PADDR: usize = 0x30000000;
+const ITCM_PADDR: usize = 0x32000000;
 const DTCM_SIZE: usize = 0x1000000;
 const DTCM_PADDR: usize = 0x34000000;
 
