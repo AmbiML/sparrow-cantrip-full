f608e919139768f8493f01d2c00152402706ab56
Sam Leffler <sleffler@google.com>
2022-05-06 19:34:02 +0000

    cantrip-os-common: make CAmkES-specific support controlled by a feature
    The slot allocator has CAmkES-specific support that will not build for
    a non-CAmkES target. Add a "camkes_support" feature flag that controls
    whether the slot allocator is included. This is enabled by default but
    allows users like cantrip-os-rootserver to workaround the problem.
    
    Change-Id: I2decb9fc794c215455e223d17c24c6a19cc52d56
    GitOrigin-RevId: d7a3811b4a9a000b0f34fbc2abdfc628250f64d4

--- apps/system/components/kata-os-common/src/lib.rs
+++ apps/system/components/kata-os-common/src/lib.rs
@@ -5,5 +5,6 @@ pub extern crate model;
 pub extern crate allocator;
 pub extern crate logger;
 pub extern crate panic;
+#[cfg(feature = "camkes_support")]
 pub extern crate slot_allocator;
 pub extern crate sel4_sys;
