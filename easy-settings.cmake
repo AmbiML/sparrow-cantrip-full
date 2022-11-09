a860c1a8799ebcdef78bbae78849f06d05c41854
Sam Leffler <sleffler@google.com>
2022-11-09 18:50:26 +0000

    cantrip: hoist platform-dependent stuff out of easy-settings.cmake
    Remove/change various settings to simplify multi-platform support:
    - remove PLATFORM (must be set on cmake comamnd line)
    - remove RUST_TARGET (must be set on cmake command line)
    - remove KernelSel4Arch (seL4 sets this based on PLATFORM)
    - remove UseRiscvBBL (seL4 per-platform config sets this)
    - change SIMULATION (default off as we manage simulation)
    
    Change-Id: If4d0c25ba32681f4508a73b10207d80c1e29e7c2
    GitOrigin-RevId: 5a1177215e5cc7f78a47403c36a72dbcf07010bc

--- easy-settings.cmake
+++ easy-settings.cmake
@@ -12,21 +12,30 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# NB: PLATFORM, CROSS_COMPILER_PREFIX, and RUST_TARGET are
+#   expected to be set one the cmake command line
+if(NOT DEFINED PLATFORM)
+  message (SEND_ERROR "`PLATFORM` is not defined")
+endif()
+if(NOT DEFINED CROSS_COMPILER_PREFIX)
+  message (SEND_ERROR "`CROSS_COMPILER_PREFIX` is not defined")
+endif()
+if(NOT DEFINED RUST_TARGET)
+  message (SEND_ERROR "`RUST_TARGET` is not defined")
+endif()
+
 set(CAMKES_APP "system" CACHE STRING "The one and only CAmkES application in this project")
 #set(CAPDL_LOADER_APP "capdl-loader-app" CACHE STRING "")
 set(CAPDL_LOADER_APP "cantrip-os-rootserver" CACHE STRING "")
 
-set(PLATFORM "sparrow" CACHE STRING "The one and only seL4 platform for Sparrow")
-set(RUST_TARGET "riscv32imac-unknown-none-elf" CACHE STRING "Rust target_arch")
-set(KernelSel4Arch "riscv32" CACHE STRING "Specifies 32-bit branch of the seL4 spike platform")
 set(KernelIsMCS ON CACHE BOOL "Enable seL4 MCS support")
 set(KernelPrinting ON CACHE BOOL "Enable seL4 console output support")
 set(CAmkESDefaultHeapSize "8192" CACHE STRING "CAmkES per-component heap size (bytes)")
 
 set(LibUtilsDefaultZfLogLevel 5 CACHE STRING "seL4 internal logging level (0-5).")
-set(SIMULATION ON CACHE BOOL "Whether to build simulate script")
+set(SIMULATION OFF CACHE BOOL "Whether to build simulate script")
 set(RELEASE OFF CACHE BOOL "Performance optimized build")
-set(UseRiscVBBL OFF CACHE BOOL "Whether to use bbl")
+# NB: UseRiscVBBL is set in the platform config
 
 set(KernelNumDomains 1 CACHE STRING "How many scheduling domains to build for")
 set(KernelDomainSchedule "${CMAKE_CURRENT_LIST_DIR}/kernel/round_robin_domain.c" CACHE INTERNAL "Domain scheduler algorithm")
