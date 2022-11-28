AmbiML/sparrow-kata-full

Merge "SecurityCoordinator: cleanup interface (part 1)"
GitOrigin-RevId: bb5f089cdd9edac2b5d310439adbd9d3948ac858

apps/system/components/SecurityCoordinator/kata-security-component/src/run.rs
@@ -8,8 +8,11 @@ use kata_allocator;
 use kata_logger::KataLogger;
 #[cfg(not(test))]
 extern crate kata_panic;
-use kata_security_common::*;
 use kata_security_coordinator::KATA_SECURITY;
+use kata_security_interface::SecurityCoordinatorInterface;
+use kata_security_interface::SecurityReplyData;
+use kata_security_interface::SecurityRequest;
+use kata_security_interface::SecurityRequestError;
 use log::trace;
 
 #[no_mangle]

apps/system/components/SecurityCoordinator/kata-security-coordinator/src/fakeimpl/mod.rs
@@ -3,7 +3,17 @@
 extern crate alloc;
 use alloc::string::{String, ToString};
 use hashbrown::HashMap;
-use kata_security_common::*;
+use kata_security_interface::DeleteKeyRequest;
+use kata_security_interface::GetManifestRequest;
+use kata_security_interface::LoadApplicationRequest;
+use kata_security_interface::LoadModelRequest;
+use kata_security_interface::ReadKeyRequest;
+use kata_security_interface::SecurityCoordinatorInterface;
+use kata_security_interface::SecurityRequest;
+use kata_security_interface::SecurityRequestError;
+use kata_security_interface::SizeBufferRequest;
+use kata_security_interface::UninstallRequest;
+use kata_security_interface::WriteKeyRequest;
 use kata_storage_interface::{KeyValueData, KEY_VALUE_DATA_SIZE};
 use log::trace;
 use postcard;
@@ -123,7 +133,7 @@ impl SecurityCoordinatorInterface for FakeSecurityCoordinator {
                 Ok(())
             }
             SecurityRequest::SrGetManifest => {
-                let request = postcard::from_bytes::<SizeBufferRequest>(&request_buffer[..])
+                let request = postcard::from_bytes::<GetManifestRequest>(&request_buffer[..])
                     .map_err(deserialize_failure)?;
                 trace!("GET MANIFEST bundle_id {}", request.bundle_id);
                 let bundle = self.get_bundle(&request.bundle_id)?;

apps/system/components/SecurityCoordinator/kata-security-coordinator/src/lib.rs
@@ -6,7 +6,9 @@
 
 extern crate alloc;
 use alloc::boxed::Box;
-use kata_security_common::*;
+use kata_security_interface::SecurityCoordinatorInterface;
+use kata_security_interface::SecurityRequest;
+use kata_security_interface::SecurityRequestError;
 
 #[cfg(all(feature = "fake", feature = "sel4"))]
 compile_error!("features \"fake\" and \"sel4\" are mutually exclusive");

apps/system/components/StorageManager/kata-storage-interface/src/lib.rs
@@ -4,6 +4,8 @@
 
 use core::str;
 use cstr_core::CString;
+use kata_security_interface::SecurityRequestError;
+use postcard;
 
 // TODO(sleffler): temp constraint on value part of key-value pairs
 pub const KEY_VALUE_DATA_SIZE: usize = 100;
@@ -20,10 +22,32 @@ pub enum StorageError {
     KeyInvalid,
     ValueInvalid,
     SerializeFailed,
+    UnknownSecurityError,
     // Generic errors.
     ReadFailed,
     WriteFailed,
     DeleteFailed,
+}
+
+impl From<postcard::Error> for StorageError {
+    fn from(_err: postcard::Error) -> StorageError {
+        StorageError::SerializeFailed
+    }
+}
+impl From<SecurityRequestError> for StorageError {
+    fn from(err: SecurityRequestError) -> StorageError {
+        match err {
+            SecurityRequestError::SreSuccess => StorageError::Success,
+            SecurityRequestError::SreBundleNotFound => StorageError::BundleNotFound,
+            SecurityRequestError::SreKeyNotFound => StorageError::KeyNotFound,
+            SecurityRequestError::SreValueInvalid => StorageError::ValueInvalid,
+            SecurityRequestError::SreKeyInvalid => StorageError::KeyInvalid,
+            SecurityRequestError::SreReadFailed => StorageError::ReadFailed,
+            SecurityRequestError::SreWriteFailed => StorageError::WriteFailed,
+            SecurityRequestError::SreDeleteFailed => StorageError::DeleteFailed,
+            _ => StorageError::UnknownSecurityError,  // NB: cannot happen
+        }
+    }
 }
 
 pub trait StorageManagerInterface {
@@ -51,17 +75,20 @@ pub enum StorageManagerError {
     SmeDeleteFailed,
 }
 
+impl From<cstr_core::NulError> for StorageManagerError {
+    fn from(_err: cstr_core::NulError) -> StorageManagerError {
+        StorageManagerError::SmeKeyInvalid
+    }
+}
+
 #[inline]
 #[allow(dead_code)]
 pub fn kata_storage_delete(key: &str) -> Result<(), StorageManagerError> {
     // NB: this assumes the StorageManager component is named "storage".
     extern "C" {
-        pub fn storage_delete(
-            c_key: *const cstr_core::c_char
-        ) -> StorageManagerError;
+        pub fn storage_delete(c_key: *const cstr_core::c_char) -> StorageManagerError;
     }
-    let cstr = CString::new(key)
-        .map_err(|_| StorageManagerError::SmeKeyInvalid)?;
+    let cstr = CString::new(key)?;
     match unsafe { storage_delete(cstr.as_ptr()) } {
         StorageManagerError::SmeSuccess => Ok(()),
         status => Err(status),
@@ -77,8 +104,7 @@ pub fn kata_storage_read(key: &str) -> Result<KeyValueData, StorageManagerError>
             c_raw_value: *mut KeyValueData,
         ) -> StorageManagerError;
     }
-    let cstr = CString::new(key)
-        .map_err(|_| StorageManagerError::SmeKeyInvalid)?;
+    let cstr = CString::new(key)?;
     let value = &mut [0u8; KEY_VALUE_DATA_SIZE];
     match unsafe { storage_read(cstr.as_ptr(), value as *mut _) } {
         StorageManagerError::SmeSuccess => Ok(*value),
@@ -96,8 +122,7 @@ pub fn kata_storage_write(key: &str, value: &[u8]) -> Result<(), StorageManagerE
             c_raw_value: *const u8,
         ) -> StorageManagerError;
     }
-    let cstr = CString::new(key)
-        .map_err(|_| StorageManagerError::SmeKeyInvalid)?;
+    let cstr = CString::new(key)?;
     match unsafe { storage_write(cstr.as_ptr(), value.len(), value.as_ptr()) } {
         StorageManagerError::SmeSuccess => Ok(()),
         status => Err(status),

apps/system/components/StorageManager/kata-storage-manager/src/lib.rs
@@ -4,16 +4,22 @@
 
 extern crate alloc;
 use alloc::string::String;
-use kata_security_common::*;
-use kata_storage_interface::{KeyValueData, KEY_VALUE_DATA_SIZE};
+use kata_security_interface::kata_security_request;
+use kata_security_interface::DeleteKeyRequest;
+use kata_security_interface::ReadKeyRequest;
+use kata_security_interface::SecurityRequest;
+use kata_security_interface::WriteKeyRequest;
+use kata_security_interface::SECURITY_REPLY_DATA_SIZE;
+use kata_security_interface::SECURITY_REQUEST_DATA_SIZE;
 use kata_storage_interface::StorageError;
 use kata_storage_interface::StorageManagerInterface;
+use kata_storage_interface::{KeyValueData, KEY_VALUE_DATA_SIZE};
 use log::trace;
 use postcard;
 
 // NB: KATA_STORAGE cannot be used before setup is completed with a call to init()
 #[cfg(not(test))]
-pub static mut KATA_STORAGE: KataStorageManager = KataStorageManager{};
+pub static mut KATA_STORAGE: KataStorageManager = KataStorageManager {};
 
 // KataStorageManager bundles an instance of the StorageManager that operates
 // on KataOS interfaces. There is a two-step dance to setup an instance because
@@ -23,11 +29,6 @@ impl StorageManagerInterface for KataStorageManager {
     fn read(&self, bundle_id: &str, key: &str) -> Result<KeyValueData, StorageError> {
         trace!("read bundle_id:{} key:{}", bundle_id, key);
 
-        fn serialize_failure(e: postcard::Error) -> StorageError {
-            trace!("read: serialize failure {:?}", e);
-            StorageError::SerializeFailed
-        }
-
         // Send request to Security Core via SecurityCoordinator
         let mut request = [0u8; SECURITY_REQUEST_DATA_SIZE];
         let _ = postcard::to_slice(
@@ -36,17 +37,13 @@ impl StorageManagerInterface for KataStorageManager {
                 key: String::from(key),
             },
             &mut request[..],
-        )
-        .map_err(serialize_failure)?;
+        )?;
         let result = &mut [0u8; SECURITY_REPLY_DATA_SIZE];
-        match kata_security_request(SecurityRequest::SrReadKey, &request, result) {
-            SecurityRequestError::SreSuccess => {
-                let mut keyval = [0u8; KEY_VALUE_DATA_SIZE];
-                keyval.copy_from_slice(&result[..KEY_VALUE_DATA_SIZE]);
-                Ok(keyval)
-            }
-            e => Err(map_security_request_error(e, StorageError::ReadFailed)),
-        }
+        let _ = kata_security_request(SecurityRequest::SrReadKey, &request, result)?;
+        // NB: must copy into KeyValueData for now
+        let mut keyval = [0u8; KEY_VALUE_DATA_SIZE];
+        keyval.copy_from_slice(&result[..KEY_VALUE_DATA_SIZE]);
+        Ok(keyval)
     }
     fn write(&self, bundle_id: &str, key: &str, value: &[u8]) -> Result<(), StorageError> {
         trace!(
@@ -55,11 +52,6 @@ impl StorageManagerInterface for KataStorageManager {
             key,
             value
         );
-
-        fn serialize_failure(e: postcard::Error) -> StorageError {
-            trace!("write: serialize failure {:?}", e);
-            StorageError::SerializeFailed
-        }
 
         // Send request to Security Core via SecurityCoordinator
         let mut request = [0u8; SECURITY_REQUEST_DATA_SIZE];
@@ -70,21 +62,13 @@ impl StorageManagerInterface for KataStorageManager {
                 value: value,
             },
             &mut request[..],
-        )
-        .map_err(serialize_failure)?;
+        )?;
         let result = &mut [0u8; SECURITY_REPLY_DATA_SIZE];
-        match kata_security_request(SecurityRequest::SrWriteKey, &request, result) {
-            SecurityRequestError::SreSuccess => Ok(()),
-            e => Err(map_security_request_error(e, StorageError::WriteFailed)),
-        }
+        kata_security_request(SecurityRequest::SrWriteKey, &request, result)?;
+        Ok(())
     }
     fn delete(&self, bundle_id: &str, key: &str) -> Result<(), StorageError> {
         trace!("delete bundle_id:{} key:{}", bundle_id, key);
-
-        fn serialize_failure(e: postcard::Error) -> StorageError {
-            trace!("delete: serialize failure {:?}", e);
-            StorageError::SerializeFailed
-        }
 
         // Send request to Security Core via SecurityCoordinator
         let mut request = [0u8; SECURITY_REQUEST_DATA_SIZE];
@@ -94,22 +78,9 @@ impl StorageManagerInterface for KataStorageManager {
                 key: String::from(key),
             },
             &mut request[..],
-        )
-        .map_err(serialize_failure)?;
+        )?;
         let result = &mut [0u8; SECURITY_REPLY_DATA_SIZE];
-        match kata_security_request(SecurityRequest::SrDeleteKey, &request, result) {
-            SecurityRequestError::SreSuccess => Ok(()),
-            e => Err(map_security_request_error(e, StorageError::DeleteFailed)),
-        }
-    }
-}
-
-// Maps a SecuritRequestError to a StorageError.
-fn map_security_request_error(sre: SecurityRequestError, def: StorageError) -> StorageError {
-    match sre {
-        SecurityRequestError::SreSuccess => StorageError::Success,
-        SecurityRequestError::SreBundleNotFound => StorageError::BundleNotFound,
-        SecurityRequestError::SreKeyNotFound => StorageError::KeyNotFound,
-        _ => def,
+        kata_security_request(SecurityRequest::SrDeleteKey, &request, result)?;
+        Ok(())
     }
 }

AmbiML/sparrow-cantrip-full

apps/system/components/SecurityCoordinator/kata-security-component/src/run.rs
@@ -8,8 +8,11 @@ use cantrip_allocator;
 use cantrip_logger::CantripLogger;
 #[cfg(not(test))]
 extern crate cantrip_panic;
-use cantrip_security_common::*;
 use cantrip_security_coordinator::CANTRIP_SECURITY;
+use cantrip_security_interface::SecurityCoordinatorInterface;
+use cantrip_security_interface::SecurityReplyData;
+use cantrip_security_interface::SecurityRequest;
+use cantrip_security_interface::SecurityRequestError;
 use log::trace;
 
 #[no_mangle]

apps/system/components/SecurityCoordinator/kata-security-coordinator/src/fakeimpl/mod.rs
@@ -3,7 +3,17 @@
 extern crate alloc;
 use alloc::string::{String, ToString};
 use hashbrown::HashMap;
-use cantrip_security_common::*;
+use cantrip_security_interface::DeleteKeyRequest;
+use cantrip_security_interface::GetManifestRequest;
+use cantrip_security_interface::LoadApplicationRequest;
+use cantrip_security_interface::LoadModelRequest;
+use cantrip_security_interface::ReadKeyRequest;
+use cantrip_security_interface::SecurityCoordinatorInterface;
+use cantrip_security_interface::SecurityRequest;
+use cantrip_security_interface::SecurityRequestError;
+use cantrip_security_interface::SizeBufferRequest;
+use cantrip_security_interface::UninstallRequest;
+use cantrip_security_interface::WriteKeyRequest;
 use cantrip_storage_interface::{KeyValueData, KEY_VALUE_DATA_SIZE};
 use log::trace;
 use postcard;
@@ -123,7 +133,7 @@ impl SecurityCoordinatorInterface for FakeSecurityCoordinator {
                 Ok(())
             }
             SecurityRequest::SrGetManifest => {
-                let request = postcard::from_bytes::<SizeBufferRequest>(&request_buffer[..])
+                let request = postcard::from_bytes::<GetManifestRequest>(&request_buffer[..])
                     .map_err(deserialize_failure)?;
                 trace!("GET MANIFEST bundle_id {}", request.bundle_id);
                 let bundle = self.get_bundle(&request.bundle_id)?;

apps/system/components/SecurityCoordinator/kata-security-coordinator/src/lib.rs
@@ -6,7 +6,9 @@
 
 extern crate alloc;
 use alloc::boxed::Box;
-use cantrip_security_common::*;
+use cantrip_security_interface::SecurityCoordinatorInterface;
+use cantrip_security_interface::SecurityRequest;
+use cantrip_security_interface::SecurityRequestError;
 
 #[cfg(all(feature = "fake", feature = "sel4"))]
 compile_error!("features \"fake\" and \"sel4\" are mutually exclusive");

apps/system/components/StorageManager/kata-storage-interface/src/lib.rs
@@ -4,6 +4,8 @@
 
 use core::str;
 use cstr_core::CString;
+use cantrip_security_interface::SecurityRequestError;
+use postcard;
 
 // TODO(sleffler): temp constraint on value part of key-value pairs
 pub const KEY_VALUE_DATA_SIZE: usize = 100;
@@ -20,10 +22,32 @@ pub enum StorageError {
     KeyInvalid,
     ValueInvalid,
     SerializeFailed,
+    UnknownSecurityError,
     // Generic errors.
     ReadFailed,
     WriteFailed,
     DeleteFailed,
+}
+
+impl From<postcard::Error> for StorageError {
+    fn from(_err: postcard::Error) -> StorageError {
+        StorageError::SerializeFailed
+    }
+}
+impl From<SecurityRequestError> for StorageError {
+    fn from(err: SecurityRequestError) -> StorageError {
+        match err {
+            SecurityRequestError::SreSuccess => StorageError::Success,
+            SecurityRequestError::SreBundleNotFound => StorageError::BundleNotFound,
+            SecurityRequestError::SreKeyNotFound => StorageError::KeyNotFound,
+            SecurityRequestError::SreValueInvalid => StorageError::ValueInvalid,
+            SecurityRequestError::SreKeyInvalid => StorageError::KeyInvalid,
+            SecurityRequestError::SreReadFailed => StorageError::ReadFailed,
+            SecurityRequestError::SreWriteFailed => StorageError::WriteFailed,
+            SecurityRequestError::SreDeleteFailed => StorageError::DeleteFailed,
+            _ => StorageError::UnknownSecurityError,  // NB: cannot happen
+        }
+    }
 }
 
 pub trait StorageManagerInterface {
@@ -51,17 +75,20 @@ pub enum StorageManagerError {
     SmeDeleteFailed,
 }
 
+impl From<cstr_core::NulError> for StorageManagerError {
+    fn from(_err: cstr_core::NulError) -> StorageManagerError {
+        StorageManagerError::SmeKeyInvalid
+    }
+}
+
 #[inline]
 #[allow(dead_code)]
 pub fn cantrip_storage_delete(key: &str) -> Result<(), StorageManagerError> {
     // NB: this assumes the StorageManager component is named "storage".
     extern "C" {
-        pub fn storage_delete(
-            c_key: *const cstr_core::c_char
-        ) -> StorageManagerError;
+        pub fn storage_delete(c_key: *const cstr_core::c_char) -> StorageManagerError;
     }
-    let cstr = CString::new(key)
-        .map_err(|_| StorageManagerError::SmeKeyInvalid)?;
+    let cstr = CString::new(key)?;
     match unsafe { storage_delete(cstr.as_ptr()) } {
         StorageManagerError::SmeSuccess => Ok(()),
         status => Err(status),
@@ -77,8 +104,7 @@ pub fn cantrip_storage_read(key: &str) -> Result<KeyValueData, StorageManagerErr
             c_raw_value: *mut KeyValueData,
         ) -> StorageManagerError;
     }
-    let cstr = CString::new(key)
-        .map_err(|_| StorageManagerError::SmeKeyInvalid)?;
+    let cstr = CString::new(key)?;
     let value = &mut [0u8; KEY_VALUE_DATA_SIZE];
     match unsafe { storage_read(cstr.as_ptr(), value as *mut _) } {
         StorageManagerError::SmeSuccess => Ok(*value),
@@ -96,8 +122,7 @@ pub fn cantrip_storage_write(key: &str, value: &[u8]) -> Result<(), StorageManag
             c_raw_value: *const u8,
         ) -> StorageManagerError;
     }
-    let cstr = CString::new(key)
-        .map_err(|_| StorageManagerError::SmeKeyInvalid)?;
+    let cstr = CString::new(key)?;
     match unsafe { storage_write(cstr.as_ptr(), value.len(), value.as_ptr()) } {
         StorageManagerError::SmeSuccess => Ok(()),
         status => Err(status),

apps/system/components/StorageManager/kata-storage-manager/src/lib.rs
@@ -4,16 +4,22 @@
 
 extern crate alloc;
 use alloc::string::String;
-use cantrip_security_common::*;
-use cantrip_storage_interface::{KeyValueData, KEY_VALUE_DATA_SIZE};
+use cantrip_security_interface::cantrip_security_request;
+use cantrip_security_interface::DeleteKeyRequest;
+use cantrip_security_interface::ReadKeyRequest;
+use cantrip_security_interface::SecurityRequest;
+use cantrip_security_interface::WriteKeyRequest;
+use cantrip_security_interface::SECURITY_REPLY_DATA_SIZE;
+use cantrip_security_interface::SECURITY_REQUEST_DATA_SIZE;
 use cantrip_storage_interface::StorageError;
 use cantrip_storage_interface::StorageManagerInterface;
+use cantrip_storage_interface::{KeyValueData, KEY_VALUE_DATA_SIZE};
 use log::trace;
 use postcard;
 
 // NB: CANTRIP_STORAGE cannot be used before setup is completed with a call to init()
 #[cfg(not(test))]
-pub static mut CANTRIP_STORAGE: CantripStorageManager = CantripStorageManager{};
+pub static mut CANTRIP_STORAGE: CantripStorageManager = CantripStorageManager {};
 
 // CantripStorageManager bundles an instance of the StorageManager that operates
 // on CantripOS interfaces. There is a two-step dance to setup an instance because
@@ -23,11 +29,6 @@ impl StorageManagerInterface for CantripStorageManager {
     fn read(&self, bundle_id: &str, key: &str) -> Result<KeyValueData, StorageError> {
         trace!("read bundle_id:{} key:{}", bundle_id, key);
 
-        fn serialize_failure(e: postcard::Error) -> StorageError {
-            trace!("read: serialize failure {:?}", e);
-            StorageError::SerializeFailed
-        }
-
         // Send request to Security Core via SecurityCoordinator
         let mut request = [0u8; SECURITY_REQUEST_DATA_SIZE];
         let _ = postcard::to_slice(
@@ -36,17 +37,13 @@ impl StorageManagerInterface for CantripStorageManager {
                 key: String::from(key),
             },
             &mut request[..],
-        )
-        .map_err(serialize_failure)?;
+        )?;
         let result = &mut [0u8; SECURITY_REPLY_DATA_SIZE];
-        match cantrip_security_request(SecurityRequest::SrReadKey, &request, result) {
-            SecurityRequestError::SreSuccess => {
-                let mut keyval = [0u8; KEY_VALUE_DATA_SIZE];
-                keyval.copy_from_slice(&result[..KEY_VALUE_DATA_SIZE]);
-                Ok(keyval)
-            }
-            e => Err(map_security_request_error(e, StorageError::ReadFailed)),
-        }
+        let _ = cantrip_security_request(SecurityRequest::SrReadKey, &request, result)?;
+        // NB: must copy into KeyValueData for now
+        let mut keyval = [0u8; KEY_VALUE_DATA_SIZE];
+        keyval.copy_from_slice(&result[..KEY_VALUE_DATA_SIZE]);
+        Ok(keyval)
     }
     fn write(&self, bundle_id: &str, key: &str, value: &[u8]) -> Result<(), StorageError> {
         trace!(
@@ -55,11 +52,6 @@ impl StorageManagerInterface for CantripStorageManager {
             key,
             value
         );
-
-        fn serialize_failure(e: postcard::Error) -> StorageError {
-            trace!("write: serialize failure {:?}", e);
-            StorageError::SerializeFailed
-        }
 
         // Send request to Security Core via SecurityCoordinator
         let mut request = [0u8; SECURITY_REQUEST_DATA_SIZE];
@@ -70,21 +62,13 @@ impl StorageManagerInterface for CantripStorageManager {
                 value: value,
             },
             &mut request[..],
-        )
-        .map_err(serialize_failure)?;
+        )?;
         let result = &mut [0u8; SECURITY_REPLY_DATA_SIZE];
-        match cantrip_security_request(SecurityRequest::SrWriteKey, &request, result) {
-            SecurityRequestError::SreSuccess => Ok(()),
-            e => Err(map_security_request_error(e, StorageError::WriteFailed)),
-        }
+        cantrip_security_request(SecurityRequest::SrWriteKey, &request, result)?;
+        Ok(())
     }
     fn delete(&self, bundle_id: &str, key: &str) -> Result<(), StorageError> {
         trace!("delete bundle_id:{} key:{}", bundle_id, key);
-
-        fn serialize_failure(e: postcard::Error) -> StorageError {
-            trace!("delete: serialize failure {:?}", e);
-            StorageError::SerializeFailed
-        }
 
         // Send request to Security Core via SecurityCoordinator
         let mut request = [0u8; SECURITY_REQUEST_DATA_SIZE];
@@ -94,22 +78,9 @@ impl StorageManagerInterface for CantripStorageManager {
                 key: String::from(key),
             },
             &mut request[..],
-        )
-        .map_err(serialize_failure)?;
+        )?;
         let result = &mut [0u8; SECURITY_REPLY_DATA_SIZE];
-        match cantrip_security_request(SecurityRequest::SrDeleteKey, &request, result) {
-            SecurityRequestError::SreSuccess => Ok(()),
-            e => Err(map_security_request_error(e, StorageError::DeleteFailed)),
-        }
-    }
-}
-
-// Maps a SecuritRequestError to a StorageError.
-fn map_security_request_error(sre: SecurityRequestError, def: StorageError) -> StorageError {
-    match sre {
-        SecurityRequestError::SreSuccess => StorageError::Success,
-        SecurityRequestError::SreBundleNotFound => StorageError::BundleNotFound,
-        SecurityRequestError::SreKeyNotFound => StorageError::KeyNotFound,
-        _ => def,
+        cantrip_security_request(SecurityRequest::SrDeleteKey, &request, result)?;
+        Ok(())
     }
 }