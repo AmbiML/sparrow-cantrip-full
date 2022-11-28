AmbiML/sparrow-kata-full

Merge "Remove the Kanji prompt character and add a printf when the repl starts up so we can see it in the Renode log."
GitOrigin-RevId: cd401621c05352bbdd10a70fbf788c58bc2efa10

apps/system/components/DebugConsole/kata-shell/src/lib.rs
@@ -43,10 +43,10 @@ impl From<fmt::Error> for CommandError {
 
 /// Read-eval-print loop for the DebugConsole command line interface.
 pub fn repl(output: &mut dyn io::Write, input: &mut dyn io::Read) -> ! {
-    let mut line_reader = LineReader::new();
+  let _ = write!(output, "DebugConsole::repl()\n");
+  let mut line_reader = LineReader::new();
     loop {
-        // The PROMPT is the Kanji character for the word "form", or "kata."
-        const PROMPT: &str = "形＞ ";
+        const PROMPT: &str = "KATA_PROMPT> ";
         let _ = output.write_str(PROMPT);
         match line_reader.read_line(output, input) {
             Ok(cmdline) => dispatch_command(cmdline, output),

AmbiML/sparrow-cantrip-full

apps/system/components/DebugConsole/kata-shell/src/lib.rs
@@ -43,10 +43,10 @@ impl From<fmt::Error> for CommandError {
 
 /// Read-eval-print loop for the DebugConsole command line interface.
 pub fn repl(output: &mut dyn io::Write, input: &mut dyn io::Read) -> ! {
-    let mut line_reader = LineReader::new();
+  let _ = write!(output, "DebugConsole::repl()\n");
+  let mut line_reader = LineReader::new();
     loop {
-        // The PROMPT is the Kanji character for the word "form", or "cantrip."
-        const PROMPT: &str = "形＞ ";
+        const PROMPT: &str = "CANTRIP_PROMPT> ";
         let _ = output.write_str(PROMPT);
         match line_reader.read_line(output, input) {
             Ok(cmdline) => dispatch_command(cmdline, output),