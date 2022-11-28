AmbiML/sparrow-kata-full

Merge "SDKRuntime: Introduce skeleton implementation"
GitOrigin-RevId: 4c147d204f782881662e3d30b36f848906ae51d3

apps/system/components/DebugConsole/kata-shell/src/test_sdk_runtime.rs
@@ -1,0 +1,45 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//! SDK Runtime shell test commands
+
+use crate::CmdFn;
+use crate::CommandError;
+use crate::HashMap;
+use core::fmt::Write;
+
+use kata_io as io;
+
+use kata_sdk_interface::kata_sdk_ping;
+
+pub fn add_cmds(cmds: &mut HashMap<&str, CmdFn>) {
+    cmds.extend([("test_sdkping", sdk_ping_command as CmdFn)]);
+}
+
+fn sdk_ping_command(
+    _args: &mut dyn Iterator<Item = &str>,
+    _input: &mut dyn io::BufRead,
+    output: &mut dyn io::Write,
+    _builtin_cpio: &[u8],
+) -> Result<(), CommandError> {
+    match kata_sdk_ping() {
+        Ok(()) => {
+            writeln!(output, "pong received")?;
+        }
+        Err(sdkerror) => {
+            writeln!(output, "ping failed: {:?}", sdkerror)?;
+        }
+    }
+    Ok(())
+}

apps/system/components/SDKRuntime/kata-sdk-component/src/run.rs
@@ -1,0 +1,51 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/*!
+ * KataOS SDK Manager CAmkES component support routines.
+ *
+ * Functions defined here are entrypoints defined by the CAmkES component
+ * definition in SDKRuntime.camkes, and bind the C entry points to Rust by
+ * calling Rust methods in the SDKRuntimeInterface impl, KATA_SDK.
+ *
+ * This is the lowest level entry point from C to Rust in CAmkES.
+ */
+
+#![no_std]
+#![allow(clippy::missing_safety_doc)]
+
+extern crate alloc;
+use kata_os_common::camkes::Camkes;
+use kata_sdk_interface::SDKRuntimeError;
+use kata_sdk_interface::SDKRuntimeInterface;
+use kata_sdk_runtime::KATA_SDK;
+
+static mut CAMKES: Camkes = Camkes::new("SDKRuntime");
+
+/// CAmkES component pre-init method.
+///
+/// We use this to initialize our Rust heap, logger, etc.
+#[no_mangle]
+pub unsafe extern "C" fn pre_init() {
+    static mut HEAP_MEMORY: [u8; 8 * 1024] = [0; 8 * 1024];
+    CAMKES.pre_init(log::LevelFilter::Trace, &mut HEAP_MEMORY);
+}
+
+/// CAmkES sdk_ping method.
+///
+/// See also the component interface definition called
+/// `SDKRuntimeInterface.camkes` outside of this crate. Since this is a C
+/// function, we must use the C enum for error codes.
+#[no_mangle]
+pub unsafe extern "C" fn sdk_runtime_sdk_ping() -> SDKRuntimeError { KATA_SDK.ping().into() }

apps/system/components/SDKRuntime/kata-sdk-interface/src/lib.rs
@@ -1,0 +1,50 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//! KataOS SDK runtime interfaces
+
+#![cfg_attr(not(test), no_std)]
+
+pub mod error;
+
+pub use error::SDKError;
+pub use error::SDKRuntimeError;
+
+/// Rust interface for the SDKRuntime.
+///
+/// This trait defines all of the same verbs we expect to support in the component
+/// interface, for both client and server, since CAmkES does not (yet) know how
+/// to generate Rust bindings.
+///
+/// On the server side, the impl of this trait is instantiated in the component
+/// as a global mutable object where the incoming calls from the CAmkES C side
+/// are wrapped.
+///
+/// On the client side, this trait is implemented using top-level functions,
+/// wrapping their CAmkES C stubs.
+pub trait SDKRuntimeInterface {
+    /// Pings the SDK runtime, going from client to server and back via CAmkES IPC.
+    fn ping(&self) -> Result<(), SDKError>;
+}
+
+/// Rust client-side wrapper for the autogenerated CAmkES ping method.
+#[inline]
+#[allow(dead_code)]
+pub fn kata_sdk_ping() -> Result<(), SDKError> {
+    extern "C" {
+        fn sdk_runtime_sdk_ping() -> SDKRuntimeError;
+    }
+
+    unsafe { sdk_runtime_sdk_ping().into() }
+}

apps/system/components/SDKRuntime/kata-sdk-runtime/src/lib.rs
@@ -1,0 +1,35 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#![cfg_attr(not(test), no_std)]
+
+use kata_sdk_interface::error::SDKError;
+use kata_sdk_interface::SDKRuntimeInterface;
+use log::trace;
+
+#[cfg(not(test))]
+pub static mut KATA_SDK: KataSDKRuntime = KataSDKRuntime {};
+
+/// Kata OS SDK support for third-party applications, Rust core.
+///
+/// This is the actual Rust implementation of the SDK runtime component. Here's
+/// where we can encapsulate all of our Rust fanciness, away from the C
+/// bindings. This is the server-side implementation.
+pub struct KataSDKRuntime;
+impl SDKRuntimeInterface for KataSDKRuntime {
+    fn ping(&self) -> Result<(), SDKError> {
+        trace!("ping!");
+        Ok(())
+    }
+}

AmbiML/sparrow-cantrip-full

apps/system/components/DebugConsole/kata-shell/src/test_sdk_runtime.rs
@@ -1,0 +1,45 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//! SDK Runtime shell test commands
+
+use crate::CmdFn;
+use crate::CommandError;
+use crate::HashMap;
+use core::fmt::Write;
+
+use cantrip_io as io;
+
+use cantrip_sdk_interface::cantrip_sdk_ping;
+
+pub fn add_cmds(cmds: &mut HashMap<&str, CmdFn>) {
+    cmds.extend([("test_sdkping", sdk_ping_command as CmdFn)]);
+}
+
+fn sdk_ping_command(
+    _args: &mut dyn Iterator<Item = &str>,
+    _input: &mut dyn io::BufRead,
+    output: &mut dyn io::Write,
+    _builtin_cpio: &[u8],
+) -> Result<(), CommandError> {
+    match cantrip_sdk_ping() {
+        Ok(()) => {
+            writeln!(output, "pong received")?;
+        }
+        Err(sdkerror) => {
+            writeln!(output, "ping failed: {:?}", sdkerror)?;
+        }
+    }
+    Ok(())
+}

apps/system/components/SDKRuntime/kata-sdk-component/src/run.rs
@@ -1,0 +1,51 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/*!
+ * CantripOS SDK Manager CAmkES component support routines.
+ *
+ * Functions defined here are entrypoints defined by the CAmkES component
+ * definition in SDKRuntime.camkes, and bind the C entry points to Rust by
+ * calling Rust methods in the SDKRuntimeInterface impl, CANTRIP_SDK.
+ *
+ * This is the lowest level entry point from C to Rust in CAmkES.
+ */
+
+#![no_std]
+#![allow(clippy::missing_safety_doc)]
+
+extern crate alloc;
+use cantrip_os_common::camkes::Camkes;
+use cantrip_sdk_interface::SDKRuntimeError;
+use cantrip_sdk_interface::SDKRuntimeInterface;
+use cantrip_sdk_runtime::CANTRIP_SDK;
+
+static mut CAMKES: Camkes = Camkes::new("SDKRuntime");
+
+/// CAmkES component pre-init method.
+///
+/// We use this to initialize our Rust heap, logger, etc.
+#[no_mangle]
+pub unsafe extern "C" fn pre_init() {
+    static mut HEAP_MEMORY: [u8; 8 * 1024] = [0; 8 * 1024];
+    CAMKES.pre_init(log::LevelFilter::Trace, &mut HEAP_MEMORY);
+}
+
+/// CAmkES sdk_ping method.
+///
+/// See also the component interface definition called
+/// `SDKRuntimeInterface.camkes` outside of this crate. Since this is a C
+/// function, we must use the C enum for error codes.
+#[no_mangle]
+pub unsafe extern "C" fn sdk_runtime_sdk_ping() -> SDKRuntimeError { CANTRIP_SDK.ping().into() }

apps/system/components/SDKRuntime/kata-sdk-interface/src/lib.rs
@@ -1,0 +1,50 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//! CantripOS SDK runtime interfaces
+
+#![cfg_attr(not(test), no_std)]
+
+pub mod error;
+
+pub use error::SDKError;
+pub use error::SDKRuntimeError;
+
+/// Rust interface for the SDKRuntime.
+///
+/// This trait defines all of the same verbs we expect to support in the component
+/// interface, for both client and server, since CAmkES does not (yet) know how
+/// to generate Rust bindings.
+///
+/// On the server side, the impl of this trait is instantiated in the component
+/// as a global mutable object where the incoming calls from the CAmkES C side
+/// are wrapped.
+///
+/// On the client side, this trait is implemented using top-level functions,
+/// wrapping their CAmkES C stubs.
+pub trait SDKRuntimeInterface {
+    /// Pings the SDK runtime, going from client to server and back via CAmkES IPC.
+    fn ping(&self) -> Result<(), SDKError>;
+}
+
+/// Rust client-side wrapper for the autogenerated CAmkES ping method.
+#[inline]
+#[allow(dead_code)]
+pub fn cantrip_sdk_ping() -> Result<(), SDKError> {
+    extern "C" {
+        fn sdk_runtime_sdk_ping() -> SDKRuntimeError;
+    }
+
+    unsafe { sdk_runtime_sdk_ping().into() }
+}

apps/system/components/SDKRuntime/kata-sdk-runtime/src/lib.rs
@@ -1,0 +1,35 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#![cfg_attr(not(test), no_std)]
+
+use cantrip_sdk_interface::error::SDKError;
+use cantrip_sdk_interface::SDKRuntimeInterface;
+use log::trace;
+
+#[cfg(not(test))]
+pub static mut CANTRIP_SDK: CantripSDKRuntime = CantripSDKRuntime {};
+
+/// Cantrip OS SDK support for third-party applications, Rust core.
+///
+/// This is the actual Rust implementation of the SDK runtime component. Here's
+/// where we can encapsulate all of our Rust fanciness, away from the C
+/// bindings. This is the server-side implementation.
+pub struct CantripSDKRuntime;
+impl SDKRuntimeInterface for CantripSDKRuntime {
+    fn ping(&self) -> Result<(), SDKError> {
+        trace!("ping!");
+        Ok(())
+    }
+}