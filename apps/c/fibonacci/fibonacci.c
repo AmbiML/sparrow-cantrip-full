AmbiML/sparrow-kata-full

Merge "apps: Extract crt0 from C apps"
GitOrigin-RevId: af8b6e41c39a9d5d0b85cb5f7b66d986e1bc3cf9

apps/c/fibonacci/fibonacci.c
@@ -10,30 +10,8 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include <kernel/gen_config.h>
-#include <sel4/arch/syscalls.h>
-#include <stdarg.h>
+#include <kata.h>
 #include <stdint.h>
-#include <stdio.h>
-
-__thread seL4_IPCBuffer *__sel4_ipc_buffer;
-
-char minisel_tls[4096] __attribute__((__aligned__(4096)));
-
-__attribute__((naked)) void _start() {
-  asm volatile(
-      ".option push                  \n"
-      ".option norelax               \n"
-      "la gp, __global_pointer$      \n"
-      "la x4, minisel_tls            \n"
-      "addi sp,sp,-16                \n"
-      "sw a0, 12(sp)                 \n"
-      "sw a1, 8(sp)                  \n"
-      "sw a2, 4(sp)                  \n"
-      "sw a3, 0(sp)                  \n"
-      ".option pop                   \n"
-      "j main                        \n");
-}
 
 // How many Fibonacci numbers to write to the log.
 #define LOG_FIBONACCI_LIMIT 80
@@ -41,41 +19,6 @@ __attribute__((naked)) void _start() {
 #define CONFIG_TIMER_TICK_MS 5
 #define INTERRUPTS_PER_VIRT_SEC (1000 / CONFIG_TIMER_TICK_MS)
 #define INTERRUPTS_PER_WAIT (1 * INTERRUPTS_PER_VIRT_SEC)
-
-// only prints 32-bit "%x" hex values
-void minisel_printf(const char *fmt, ...) {
-#if CONFIG_PRINTING
-  va_list args;
-  va_start(args, fmt);
-  for (; *fmt; fmt++) {
-    if (*fmt == '%') {
-      fmt++;
-      if (*fmt == 'd') {
-        uint32_t arg = va_arg(args, uint32_t);
-        // TODO(sleffler): total hack
-        int printing = 0;
-        for (int d = 1000000000; d > 1; d /= 10) {
-          int n = (arg / d) % 10;
-          if (printing || n > 0) {
-            seL4_DebugPutChar('0' + n);
-            printing = 1;
-          }
-        }
-        seL4_DebugPutChar('0' + (arg % 10));
-      } else if (*fmt == 'x') {
-        uint32_t arg = va_arg(args, uint32_t);
-        for (int i = 7; i >= 0; i--) {
-          int n = (arg >> (4 * i)) & 0xF;
-          seL4_DebugPutChar(n > 9 ? 'A' + n - 10 : '0' + n);
-        }
-      }
-    } else {
-      seL4_DebugPutChar(*fmt);
-    }
-  }
-  va_end(args);
-#endif
-}
 
 typedef uint64_t interrupt_count_t;
 
@@ -134,9 +77,9 @@ void fibonacci_log(int pid, const fibonacci_state_t *fibonacci_state,
            "%llu; rdtime == %llu; virt_sec ~= %.2f\n",
            fibonacci_state->n, fibonacci_state->f1, interrupt_count, rdtime(),
            virtual_seconds(interrupt_count));
-  minisel_printf(log_buf);
+  debug_printf(log_buf);
 #else
-  minisel_printf(
+  debug_printf(
       "[%d]: "
       "n == %d; "
       "f == %x; "
@@ -153,7 +96,7 @@ int main(int pid, int a1, int a2, int a3) {
   interrupt_count_t interrupt_count = 0;
   fibonacci_state_t fibonacci_state;
   fibonacci_init(&fibonacci_state);
-  minisel_printf("\nFibonacci: pid %d\n", pid);
+  debug_printf("\nFibonacci: pid %d\n", pid);
   while (1) {
     wait(INTERRUPTS_PER_WAIT, &interrupt_count);
     if (fibonacci_state.n >= LOG_FIBONACCI_LIMIT) {

apps/c/hello/hello.c
@@ -9,62 +9,14 @@
 // using the seL4_DebugPutChar syscall and is intended as a starting
 // point for low-level tests.
 
-#include <kernel/gen_config.h>
-#include <sel4/arch/syscalls.h>
-#include <stdarg.h>
-#include <stdint.h>
-
-__thread seL4_IPCBuffer *__sel4_ipc_buffer;
-
-char minisel_tls[4096] __attribute__((__aligned__(4096)));
-
-__attribute__((naked)) void _start() {
-  asm volatile(
-      ".option push                  \n"
-      ".option norelax               \n"
-      "la gp, __global_pointer$      \n"
-      "la tp, minisel_tls            \n"
-      "lui t1,0                      \n"
-      "add t1,t1,tp                  \n"
-      "sw a0,0(t1) # __sel4_ipc_buffer>\n"
-      "addi sp,sp,-16                \n"
-      "sw a0, 12(sp)                 \n"
-      "sw a1, 8(sp)                  \n"
-      "sw a2, 4(sp)                  \n"
-      "sw a3, 0(sp)                  \n"
-      ".option pop                   \n"
-      "j main                        \n");
-}
-
-// only prints 32-bit "%x" hex values
-void minisel_printf(const char *fmt, ...) {
-#if CONFIG_PRINTING
-  va_list args;
-  va_start(args, fmt);
-  for (; *fmt; fmt++) {
-    if (*fmt == '%') {
-      fmt++;
-      if (*fmt == 'x') {
-        uint32_t arg = va_arg(args, uint32_t);
-        for (int i = 7; i >= 0; i--) {
-          int n = (arg >> (4 * i)) & 0xF;
-          seL4_DebugPutChar(n > 9 ? 'A' + n - 10 : '0' + n);
-        }
-      }
-    } else {
-      seL4_DebugPutChar(*fmt);
-    }
-  }
-  va_end(args);
-#endif
-}
+#include <kata.h>
 
 int main(int a0, int a1, int a2, int a3) {
-  minisel_printf("\nI am a C app!\n");
-  minisel_printf("a0 %x a1 %x a2 %x a3 %x\n", a0, a1, a2, a3);
-  minisel_printf("__sel4_ipc_buffer %x\n", __sel4_ipc_buffer);
+  debug_printf("\nI am a C app!\n");
+  debug_printf("a0 %x a1 %x a2 %x a3 %x\n", a0, a1, a2, a3);
+  debug_printf("__sel4_ipc_buffer %x\n", __sel4_ipc_buffer);
 
-  minisel_printf("Done, sleeping in WFI loop\n");
+  debug_printf("Done, sleeping in WFI loop\n");
   while (1) {
     asm("wfi");
   }

apps/c/libkata/include/kata.h
@@ -1,0 +1,38 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// NOLINT(build/header_guard)
+#ifndef KATA_H
+#define KATA_H
+
+#include <kernel/gen_config.h>
+#include <sel4/arch/syscalls.h>
+#include <stdarg.h>
+
+extern __thread seL4_IPCBuffer *__sel4_ipc_buffer;
+
+#ifdef CONFIG_PRINTING
+extern void _debug_printf(const char *fmt, ...);
+#define debug_printf(args...) \
+  do {                        \
+    _debug_printf(args);      \
+  } while (0)
+#else
+#define debug_printf(args...) \
+  do {                        \
+  } while (0)
+#warning Apps will not log to console because CONFIG_PRINTING is not defined!
+#endif  // CONFIG_PRINTING
+
+#endif  // KATA_H

apps/c/libkata/make/common.mk
@@ -1,0 +1,45 @@
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+MYDIR := $(dir $(realpath $(lastword $(MAKEFILE_LIST))))
+
+BUILD_TYPE ?= debug
+BUILD_ARCH ?= riscv32
+
+include $(MYDIR)/arch/$(BUILD_ARCH).mk
+include $(MYDIR)/sel4.mk
+
+ifeq ($(BUILD_TYPE),debug)
+	DEBUG := -g
+	OPT   := -O0
+else
+	DEBUG :=
+	OPT   := -O0  # TODO(jtgans): Actually optimize in a release build
+endif
+
+ROOTDIR    ?= $(MYDIR)
+BUILD_ROOT ?= $(ROOTDIR)/out/kata/$(ARCH_PREFIX)/$(BUILD_TYPE)/apps
+
+CC := $(ARCH_PREFIX)-gcc
+AS := $(ARCH_PREFIX)-as
+AR := $(ARCH_PREFIX)-ar
+LD := $(ARCH_PREFIX)-gcc
+
+CFLAGS := $(DEBUG) $(OPT) $(INCLUDES)
+CFLAGS += -march=$(ARCH) -mabi=$(ABI)
+CFLAGS += -std=gnu11 -nostdlib
+CFLAGS += -ftls-model=local-exec
+
+ASFLAGS := -march=$(ARCH) -mabi=$(ABI)
+LDFLAGS := $(DEBUG) -nostartfiles -static -nostdlib

apps/c/libkata/printf.c
@@ -1,0 +1,53 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <kata.h>
+#include <stdarg.h>
+#include <stdint.h>
+#include <stdio.h>
+
+#ifdef CONFIG_PRINTING
+// only prints 32-bit "%x" hex values
+void _debug_printf(const char *fmt, ...) {
+  va_list args;
+  va_start(args, fmt);
+  for (; *fmt; fmt++) {
+    if (*fmt == '%') {
+      fmt++;
+      if (*fmt == 'd') {
+        uint32_t arg = va_arg(args, uint32_t);
+        // TODO(sleffler): total hack
+        int printing = 0;
+        for (int d = 1000000000; d > 1; d /= 10) {
+          int n = (arg / d) % 10;
+          if (printing || n > 0) {
+            seL4_DebugPutChar('0' + n);
+            printing = 1;
+          }
+        }
+        seL4_DebugPutChar('0' + (arg % 10));
+      } else if (*fmt == 'x') {
+        uint32_t arg = va_arg(args, uint32_t);
+        for (int i = 7; i >= 0; i--) {
+          int n = (arg >> (4 * i)) & 0xF;
+          seL4_DebugPutChar(n > 9 ? 'A' + n - 10 : '0' + n);
+        }
+      }
+    } else {
+      seL4_DebugPutChar(*fmt);
+    }
+  }
+  va_end(args);
+}
+#endif  // CONFIG_PRINTING

apps/c/suicide/suicide.c
@@ -8,34 +8,11 @@
 // derefrences a null pointer to kill itself. It's primary use case is to test
 // out KataOS' fault handlers.
 
-#include <kernel/gen_config.h>
-#include <sel4/arch/syscalls.h>
-#include <stdarg.h>
-#include <stdint.h>
-
-__thread seL4_IPCBuffer *__sel4_ipc_buffer;
-
-char minisel_tls[4096] __attribute__((__aligned__(4096)));
-
-__attribute__((naked)) void _start() {
-  asm volatile(
-      ".option push                  \n"
-      ".option norelax               \n"
-      "la gp, __global_pointer$      \n"
-      "la x4, minisel_tls            \n"
-      "addi sp,sp,-16                \n"
-      "sw a0, 12(sp)                 \n"
-      "sw a1, 8(sp)                  \n"
-      "sw a2, 4(sp)                  \n"
-      "sw a3, 0(sp)                  \n"
-      ".option pop                   \n"
-      "j main                        \n");
-}
+#include <kata.h>
 
 int main(int a0, int a1, int a2, int a3) {
-#if CONFIG_PRINTING
-  seL4_DebugPutString("Goodbye, cruel world!\n");
-#endif
+  debug_printf("Goodbye, cruel world!\n");
+
   while (1) {
     char *p = 0x0;
     *p = 'g';

AmbiML/sparrow-cantrip-full

apps/c/fibonacci/fibonacci.c
@@ -10,30 +10,8 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include <kernel/gen_config.h>
-#include <sel4/arch/syscalls.h>
-#include <stdarg.h>
+#include <cantrip.h>
 #include <stdint.h>
-#include <stdio.h>
-
-__thread seL4_IPCBuffer *__sel4_ipc_buffer;
-
-char minisel_tls[4096] __attribute__((__aligned__(4096)));
-
-__attribute__((naked)) void _start() {
-  asm volatile(
-      ".option push                  \n"
-      ".option norelax               \n"
-      "la gp, __global_pointer$      \n"
-      "la x4, minisel_tls            \n"
-      "addi sp,sp,-16                \n"
-      "sw a0, 12(sp)                 \n"
-      "sw a1, 8(sp)                  \n"
-      "sw a2, 4(sp)                  \n"
-      "sw a3, 0(sp)                  \n"
-      ".option pop                   \n"
-      "j main                        \n");
-}
 
 // How many Fibonacci numbers to write to the log.
 #define LOG_FIBONACCI_LIMIT 80
@@ -41,41 +19,6 @@ __attribute__((naked)) void _start() {
 #define CONFIG_TIMER_TICK_MS 5
 #define INTERRUPTS_PER_VIRT_SEC (1000 / CONFIG_TIMER_TICK_MS)
 #define INTERRUPTS_PER_WAIT (1 * INTERRUPTS_PER_VIRT_SEC)
-
-// only prints 32-bit "%x" hex values
-void minisel_printf(const char *fmt, ...) {
-#if CONFIG_PRINTING
-  va_list args;
-  va_start(args, fmt);
-  for (; *fmt; fmt++) {
-    if (*fmt == '%') {
-      fmt++;
-      if (*fmt == 'd') {
-        uint32_t arg = va_arg(args, uint32_t);
-        // TODO(sleffler): total hack
-        int printing = 0;
-        for (int d = 1000000000; d > 1; d /= 10) {
-          int n = (arg / d) % 10;
-          if (printing || n > 0) {
-            seL4_DebugPutChar('0' + n);
-            printing = 1;
-          }
-        }
-        seL4_DebugPutChar('0' + (arg % 10));
-      } else if (*fmt == 'x') {
-        uint32_t arg = va_arg(args, uint32_t);
-        for (int i = 7; i >= 0; i--) {
-          int n = (arg >> (4 * i)) & 0xF;
-          seL4_DebugPutChar(n > 9 ? 'A' + n - 10 : '0' + n);
-        }
-      }
-    } else {
-      seL4_DebugPutChar(*fmt);
-    }
-  }
-  va_end(args);
-#endif
-}
 
 typedef uint64_t interrupt_count_t;
 
@@ -134,9 +77,9 @@ void fibonacci_log(int pid, const fibonacci_state_t *fibonacci_state,
            "%llu; rdtime == %llu; virt_sec ~= %.2f\n",
            fibonacci_state->n, fibonacci_state->f1, interrupt_count, rdtime(),
            virtual_seconds(interrupt_count));
-  minisel_printf(log_buf);
+  debug_printf(log_buf);
 #else
-  minisel_printf(
+  debug_printf(
       "[%d]: "
       "n == %d; "
       "f == %x; "
@@ -153,7 +96,7 @@ int main(int pid, int a1, int a2, int a3) {
   interrupt_count_t interrupt_count = 0;
   fibonacci_state_t fibonacci_state;
   fibonacci_init(&fibonacci_state);
-  minisel_printf("\nFibonacci: pid %d\n", pid);
+  debug_printf("\nFibonacci: pid %d\n", pid);
   while (1) {
     wait(INTERRUPTS_PER_WAIT, &interrupt_count);
     if (fibonacci_state.n >= LOG_FIBONACCI_LIMIT) {

apps/c/hello/hello.c
@@ -9,62 +9,14 @@
 // using the seL4_DebugPutChar syscall and is intended as a starting
 // point for low-level tests.
 
-#include <kernel/gen_config.h>
-#include <sel4/arch/syscalls.h>
-#include <stdarg.h>
-#include <stdint.h>
-
-__thread seL4_IPCBuffer *__sel4_ipc_buffer;
-
-char minisel_tls[4096] __attribute__((__aligned__(4096)));
-
-__attribute__((naked)) void _start() {
-  asm volatile(
-      ".option push                  \n"
-      ".option norelax               \n"
-      "la gp, __global_pointer$      \n"
-      "la tp, minisel_tls            \n"
-      "lui t1,0                      \n"
-      "add t1,t1,tp                  \n"
-      "sw a0,0(t1) # __sel4_ipc_buffer>\n"
-      "addi sp,sp,-16                \n"
-      "sw a0, 12(sp)                 \n"
-      "sw a1, 8(sp)                  \n"
-      "sw a2, 4(sp)                  \n"
-      "sw a3, 0(sp)                  \n"
-      ".option pop                   \n"
-      "j main                        \n");
-}
-
-// only prints 32-bit "%x" hex values
-void minisel_printf(const char *fmt, ...) {
-#if CONFIG_PRINTING
-  va_list args;
-  va_start(args, fmt);
-  for (; *fmt; fmt++) {
-    if (*fmt == '%') {
-      fmt++;
-      if (*fmt == 'x') {
-        uint32_t arg = va_arg(args, uint32_t);
-        for (int i = 7; i >= 0; i--) {
-          int n = (arg >> (4 * i)) & 0xF;
-          seL4_DebugPutChar(n > 9 ? 'A' + n - 10 : '0' + n);
-        }
-      }
-    } else {
-      seL4_DebugPutChar(*fmt);
-    }
-  }
-  va_end(args);
-#endif
-}
+#include <cantrip.h>
 
 int main(int a0, int a1, int a2, int a3) {
-  minisel_printf("\nI am a C app!\n");
-  minisel_printf("a0 %x a1 %x a2 %x a3 %x\n", a0, a1, a2, a3);
-  minisel_printf("__sel4_ipc_buffer %x\n", __sel4_ipc_buffer);
+  debug_printf("\nI am a C app!\n");
+  debug_printf("a0 %x a1 %x a2 %x a3 %x\n", a0, a1, a2, a3);
+  debug_printf("__sel4_ipc_buffer %x\n", __sel4_ipc_buffer);
 
-  minisel_printf("Done, sleeping in WFI loop\n");
+  debug_printf("Done, sleeping in WFI loop\n");
   while (1) {
     asm("wfi");
   }

apps/c/libkata/include/kata.h
@@ -1,0 +1,38 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// NOLINT(build/header_guard)
+#ifndef CANTRIP_H
+#define CANTRIP_H
+
+#include <kernel/gen_config.h>
+#include <sel4/arch/syscalls.h>
+#include <stdarg.h>
+
+extern __thread seL4_IPCBuffer *__sel4_ipc_buffer;
+
+#ifdef CONFIG_PRINTING
+extern void _debug_printf(const char *fmt, ...);
+#define debug_printf(args...) \
+  do {                        \
+    _debug_printf(args);      \
+  } while (0)
+#else
+#define debug_printf(args...) \
+  do {                        \
+  } while (0)
+#warning Apps will not log to console because CONFIG_PRINTING is not defined!
+#endif  // CONFIG_PRINTING
+
+#endif  // CANTRIP_H

apps/c/libkata/make/common.mk
@@ -1,0 +1,45 @@
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+MYDIR := $(dir $(realpath $(lastword $(MAKEFILE_LIST))))
+
+BUILD_TYPE ?= debug
+BUILD_ARCH ?= riscv32
+
+include $(MYDIR)/arch/$(BUILD_ARCH).mk
+include $(MYDIR)/sel4.mk
+
+ifeq ($(BUILD_TYPE),debug)
+	DEBUG := -g
+	OPT   := -O0
+else
+	DEBUG :=
+	OPT   := -O0  # TODO(jtgans): Actually optimize in a release build
+endif
+
+ROOTDIR    ?= $(MYDIR)
+BUILD_ROOT ?= $(ROOTDIR)/out/cantrip/$(ARCH_PREFIX)/$(BUILD_TYPE)/apps
+
+CC := $(ARCH_PREFIX)-gcc
+AS := $(ARCH_PREFIX)-as
+AR := $(ARCH_PREFIX)-ar
+LD := $(ARCH_PREFIX)-gcc
+
+CFLAGS := $(DEBUG) $(OPT) $(INCLUDES)
+CFLAGS += -march=$(ARCH) -mabi=$(ABI)
+CFLAGS += -std=gnu11 -nostdlib
+CFLAGS += -ftls-model=local-exec
+
+ASFLAGS := -march=$(ARCH) -mabi=$(ABI)
+LDFLAGS := $(DEBUG) -nostartfiles -static -nostdlib

apps/c/libkata/printf.c
@@ -1,0 +1,53 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <cantrip.h>
+#include <stdarg.h>
+#include <stdint.h>
+#include <stdio.h>
+
+#ifdef CONFIG_PRINTING
+// only prints 32-bit "%x" hex values
+void _debug_printf(const char *fmt, ...) {
+  va_list args;
+  va_start(args, fmt);
+  for (; *fmt; fmt++) {
+    if (*fmt == '%') {
+      fmt++;
+      if (*fmt == 'd') {
+        uint32_t arg = va_arg(args, uint32_t);
+        // TODO(sleffler): total hack
+        int printing = 0;
+        for (int d = 1000000000; d > 1; d /= 10) {
+          int n = (arg / d) % 10;
+          if (printing || n > 0) {
+            seL4_DebugPutChar('0' + n);
+            printing = 1;
+          }
+        }
+        seL4_DebugPutChar('0' + (arg % 10));
+      } else if (*fmt == 'x') {
+        uint32_t arg = va_arg(args, uint32_t);
+        for (int i = 7; i >= 0; i--) {
+          int n = (arg >> (4 * i)) & 0xF;
+          seL4_DebugPutChar(n > 9 ? 'A' + n - 10 : '0' + n);
+        }
+      }
+    } else {
+      seL4_DebugPutChar(*fmt);
+    }
+  }
+  va_end(args);
+}
+#endif  // CONFIG_PRINTING

apps/c/suicide/suicide.c
@@ -8,34 +8,11 @@
 // derefrences a null pointer to kill itself. It's primary use case is to test
 // out CantripOS' fault handlers.
 
-#include <kernel/gen_config.h>
-#include <sel4/arch/syscalls.h>
-#include <stdarg.h>
-#include <stdint.h>
-
-__thread seL4_IPCBuffer *__sel4_ipc_buffer;
-
-char minisel_tls[4096] __attribute__((__aligned__(4096)));
-
-__attribute__((naked)) void _start() {
-  asm volatile(
-      ".option push                  \n"
-      ".option norelax               \n"
-      "la gp, __global_pointer$      \n"
-      "la x4, minisel_tls            \n"
-      "addi sp,sp,-16                \n"
-      "sw a0, 12(sp)                 \n"
-      "sw a1, 8(sp)                  \n"
-      "sw a2, 4(sp)                  \n"
-      "sw a3, 0(sp)                  \n"
-      ".option pop                   \n"
-      "j main                        \n");
-}
+#include <cantrip.h>
 
 int main(int a0, int a1, int a2, int a3) {
-#if CONFIG_PRINTING
-  seL4_DebugPutString("Goodbye, cruel world!\n");
-#endif
+  debug_printf("Goodbye, cruel world!\n");
+
   while (1) {
     char *p = 0x0;
     *p = 'g';