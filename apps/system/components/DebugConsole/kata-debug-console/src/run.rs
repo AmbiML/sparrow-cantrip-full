AmbiML/sparrow-kata-full

Merge "Aligns UART API with Rust Read and Write"
GitOrigin-RevId: df407ae4bddb75b1bc43c35062947679c2b10c3a

apps/system/components/DebugConsole/kata-debug-console/src/run.rs
@@ -44,7 +44,7 @@ pub extern "C" fn pre_init() {
 #[no_mangle]
 pub extern "C" fn run() -> ! {
     trace!("run");
-    let mut tx = kata_uart_client::Tx {};
-    let mut rx = kata_uart_client::Rx {};
+    let mut tx = kata_uart_client::Tx::new();
+    let mut rx = kata_uart_client::Rx::new();
     kata_shell::repl(&mut tx, &mut rx);
 }

AmbiML/sparrow-cantrip-full

apps/system/components/DebugConsole/kata-debug-console/src/run.rs
@@ -44,7 +44,7 @@ pub extern "C" fn pre_init() {
 #[no_mangle]
 pub extern "C" fn run() -> ! {
     trace!("run");
-    let mut tx = cantrip_uart_client::Tx {};
-    let mut rx = cantrip_uart_client::Rx {};
+    let mut tx = cantrip_uart_client::Tx::new();
+    let mut rx = cantrip_uart_client::Rx::new();
     cantrip_shell::repl(&mut tx, &mut rx);
 }