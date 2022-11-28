AmbiML/sparrow-kata-full

Merge "Add StorageManager skeleton."
GitOrigin-RevId: e776e3c357ca54ad3b74212176da8a4ebc16b372

apps/system/components/ProcessManager/kata-proc-manager/src/lib.rs
@@ -4,6 +4,7 @@
 
 extern crate alloc;
 use alloc::string::String;
+use core::slice;
 use kata_proc_common::*;
 use kata_security_common::*;
 use log::trace;
@@ -89,14 +90,11 @@ impl ProcessManagerInterface for KataManagerInterface {
 
         // This is handled by the SecurityCoordinator.
         let reply = &mut [0u8; SECURITY_REPLY_DATA_SIZE];
-        match unsafe {
-            security_request(
-                SecurityRequest::SrInstall,
-                pkg_buffer_size,
-                pkg_buffer,
-                reply as *mut _,
-            )
-        } {
+        match kata_security_request(
+            SecurityRequest::SrInstall,
+            unsafe { slice::from_raw_parts(pkg_buffer, pkg_buffer_size as usize) },
+            reply,
+        ) {
             SecurityRequestError::SreSuccess => {
                 fn deserialize_failure(e: postcard::Error) -> ProcessManagerError {
                     trace!("install failed: deserialize {:?}", e);
@@ -123,14 +121,7 @@ impl ProcessManagerInterface for KataManagerInterface {
         let mut request_data = [0u8; SECURITY_REQUEST_DATA_SIZE];
         let _ = postcard::to_slice(&bundle_id, &mut request_data).map_err(serialize_failure)?;
         let reply = &mut [0u8; SECURITY_REPLY_DATA_SIZE];
-        match unsafe {
-            security_request(
-                SecurityRequest::SrUninstall,
-                request_data.len() as u32,
-                request_data.as_ptr(),
-                reply as *mut _,
-            )
-        } {
+        match kata_security_request(SecurityRequest::SrUninstall, &request_data, reply) {
             SecurityRequestError::SreSuccess => Ok(()),
             status => {
                 trace!("uninstall failed: {:?}", status);
@@ -139,7 +130,7 @@ impl ProcessManagerInterface for KataManagerInterface {
         }
     }
     fn start(&mut self, _bundle: &Bundle) -> Result<(), ProcessManagerError> {
-        // 1. Ask security core for application footprint with SizeBuffer 
+        // 1. Ask security core for application footprint with SizeBuffer
         // 2. Ask security core for manifest (maybe piggyback on SizeBuffer)
         //    and parse for necessary info (e.g. whether kv Storage is
         //    required, other privileges/capabilities)

apps/system/components/SecurityCoordinator/kata-security-common/src/lib.rs
@@ -45,6 +45,8 @@ mod mut_ptr_helper {
     }
 }
 
+// TODO(sleffler): convert String to &str
+
 // NB: SecurityRequestInstall is handled specially.
 
 // SecurityRequestUninstall
@@ -145,8 +147,8 @@ pub enum SecurityRequest {
     SrInstall,   // Install package [pkg_buffer] -> bundle_id
     SrUninstall, // Uninstall package [bundle_id]
 
-    SrSizeBuffer, // Size application image [bundle_id] -> u32
-    SrGetManifest, // Return application manifest [bundle_id] -> String
+    SrSizeBuffer,      // Size application image [bundle_id] -> u32
+    SrGetManifest,     // Return application manifest [bundle_id] -> String
     SrLoadApplication, // Load application [bundle_id]
     // TODO(sleffler): define <tag>?
     SrLoadModel, // Load ML model [bundle_id, <tag>]
@@ -166,14 +168,29 @@ pub trait SecurityCoordinatorInterface {
     ) -> Result<(), SecurityRequestError>;
 }
 
-// Camkes-generated rpc api.
-// NB: this requires the SecurityCoordinator component be named "security".
+// TODO(sleffler): try kata_security_request<T> to lower serde work
+#[inline]
 #[allow(dead_code)]
-extern "C" {
-    pub fn security_request(
-        c_request: SecurityRequest,
-        c_request_buffer_len: u32,
-        c_request_buffer: *const u8,
-        c_reply_buffer: *mut SecurityReplyData,
-    ) -> SecurityRequestError;
+pub fn kata_security_request(
+    request: SecurityRequest,
+    request_buffer: &[u8],
+    reply_buffer: &mut SecurityReplyData,
+) -> SecurityRequestError {
+    // NB: this assumes the SecurityCoordinator component is named "security".
+    extern "C" {
+        pub fn security_request(
+            c_request: SecurityRequest,
+            c_request_buffer_len: u32,
+            c_request_buffer: *const u8,
+            c_reply_buffer: *mut SecurityReplyData,
+        ) -> SecurityRequestError;
+    }
+    unsafe {
+        security_request(
+            request,
+            request_buffer.len() as u32,
+            request_buffer.as_ptr(),
+            reply_buffer as *mut _,
+        )
+    }
 }

apps/system/components/SecurityCoordinator/kata-security-coordinator/src/impl.rs
@@ -5,17 +5,17 @@ use kata_security_common::*;
 use log::trace;
 use postcard;
 
-pub struct SeL4SecurityCoordinatorInterface {
+pub struct SeL4SecurityCoordinator {
     // TODO(sleffler): mailbox ipc state
 }
-impl SeL4SecurityCoordinatorInterface {
+impl SeL4SecurityCoordinator {
     pub fn new() -> Self {
-        SeL4SecurityCoordinatorInterface {}
+        SeL4SecurityCoordinator {}
     }
 }
-pub type KataSecurityCoordinatorInterface = SeL4SecurityCoordinatorInterface;
+pub type KataSecurityCoordinatorInterface = SeL4SecurityCoordinator;
 
-impl SecurityCoordinatorInterface for SeL4SecurityCoordinatorInterface {
+impl SecurityCoordinatorInterface for SeL4SecurityCoordinator {
     fn request(
         &mut self,
         request_id: SecurityRequest,

apps/system/components/StorageManager/kata-storage-component/src/run.rs
@@ -1,0 +1,120 @@
+//! Kata OS StorageManager component support.
+
+// Code here binds the camkes component to the rust code.
+#![no_std]
+
+extern crate alloc;
+use core::slice;
+use cstr_core::CStr;
+extern crate kata_panic;
+use kata_allocator;
+use kata_logger::KataLogger;
+use kata_storage_interface::KeyValueData;
+use kata_storage_interface::StorageError;
+use kata_storage_interface::StorageManagerInterface;
+use kata_storage_interface::StorageManagerError;
+use kata_storage_manager::KATA_STORAGE;
+use log::trace;
+
+#[no_mangle]
+pub extern "C" fn pre_init() {
+    static KATA_LOGGER: KataLogger = KataLogger;
+    log::set_logger(&KATA_LOGGER).unwrap();
+    // NB: set to max; the LoggerInterface will filter
+    log::set_max_level(log::LevelFilter::Trace);
+
+    // TODO(sleffler): temp until we integrate with seL4
+    static mut HEAP_MEMORY: [u8; 8 * 1024] = [0; 8 * 1024];
+    unsafe {
+        kata_allocator::ALLOCATOR.init(HEAP_MEMORY.as_mut_ptr() as usize, HEAP_MEMORY.len());
+        trace!(
+            "setup heap: start_addr {:p} size {}",
+            HEAP_MEMORY.as_ptr(),
+            HEAP_MEMORY.len()
+        );
+    }
+}
+
+fn map_storage_error(se: StorageError, def: StorageManagerError) -> StorageManagerError {
+    match se {
+        StorageError::Success => StorageManagerError::SmeSuccess,
+        StorageError::BundleNotFound => StorageManagerError::SmeBundleNotFound,
+        StorageError::KeyNotFound => StorageManagerError::SmeKeyNotFound,
+        StorageError::KeyInvalid => StorageManagerError::SmeKeyInvalid,
+        StorageError::ValueInvalid => StorageManagerError::SmeValueInvalid,
+        _ => def,
+    }
+}
+
+// StorageInterface glue stubs.
+#[no_mangle]
+pub extern "C" fn storage_read(
+    c_key: *const cstr_core::c_char,
+    c_raw_value: *mut KeyValueData,
+) -> StorageManagerError {
+    unsafe {
+        match CStr::from_ptr(c_key).to_str() {
+            Ok(key) => {
+                // TODO(sleffler): de-badge reply cap to get bundle_id
+                match KATA_STORAGE.read("fubar", key) {
+                    // NB: no serialization, returns raw data
+                    Ok(value) => {
+                        (*c_raw_value).copy_from_slice(&value);
+                        StorageManagerError::SmeSuccess
+                    }
+                    Err(e) => map_storage_error(e, StorageManagerError::SmeReadFailed),
+                }
+            }
+            Err(e) => {
+                trace!("read: keyinvalid {:?}", e);
+                StorageManagerError::SmeKeyInvalid
+            }
+        }
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn storage_write(
+    c_key: *const cstr_core::c_char,
+    c_raw_value_len: usize,
+    c_raw_value: *const u8,
+) -> StorageManagerError {
+    unsafe {
+        match CStr::from_ptr(c_key).to_str() {
+            Ok(key) => {
+                // TODO(sleffler): de-badge reply cap to get bundle_id
+                match KATA_STORAGE.write(
+                    "fubar",
+                    key,
+                    slice::from_raw_parts(c_raw_value, c_raw_value_len),
+                ) {
+                    Ok(_) => StorageManagerError::SmeSuccess,
+                    Err(e) => map_storage_error(e, StorageManagerError::SmeWriteFailed),
+                }
+            }
+            Err(e) => {
+                trace!("write: keyinvalid {:?}", e);
+                StorageManagerError::SmeKeyInvalid
+            }
+        }
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn storage_delete(c_key: *const cstr_core::c_char) -> StorageManagerError {
+    unsafe {
+        match CStr::from_ptr(c_key).to_str() {
+            Ok(key) => {
+                // TODO(sleffler): de-badge reply cap to get bundle_id
+                match KATA_STORAGE.delete("fubar", key) {
+                    Ok(_) => StorageManagerError::SmeSuccess,
+                    Err(e) => map_storage_error(e, StorageManagerError::SmeDeleteFailed),
+                }
+            }
+            Err(e) => {
+                trace!("delete: keyinvalid {:?}", e);
+                StorageManagerError::SmeKeyInvalid
+            }
+        }
+    }
+}

apps/system/components/StorageManager/kata-storage-interface/src/lib.rs
@@ -1,0 +1,105 @@
+//! Kata OS storage management support
+
+#![cfg_attr(not(test), no_std)]
+
+use core::str;
+use cstr_core::CString;
+
+// TODO(sleffler): temp constraint on value part of key-value pairs
+pub const KEY_VALUE_DATA_SIZE: usize = 100;
+pub type KeyValueData = [u8; KEY_VALUE_DATA_SIZE];
+
+// NB: struct's marked repr(C) are processed by cbindgen to get a .h file
+//   used in camkes C interfaces.
+
+#[derive(Debug, Eq, PartialEq)]
+pub enum StorageError {
+    Success = 0,
+    BundleNotFound,
+    KeyNotFound,
+    KeyInvalid,
+    ValueInvalid,
+    SerializeFailed,
+    // Generic errors.
+    ReadFailed,
+    WriteFailed,
+    DeleteFailed,
+}
+
+pub trait StorageManagerInterface {
+    fn read(&self, bundle_id: &str, key: &str) -> Result<KeyValueData, StorageError>;
+    fn write(&self, bundle_id: &str, key: &str, value: &[u8]) -> Result<(), StorageError>;
+    fn delete(&self, bundle_id: &str, key: &str) -> Result<(), StorageError>;
+}
+
+// Public version of StorageError presented over rpc interface.
+// This is needed because the enum is exported to C users and needs to
+// be unique from other enum's.
+// TODO(sleffler): switch to single generic error space ala absl::StatusCode
+#[repr(C)]
+#[derive(Debug, Eq, PartialEq)]
+pub enum StorageManagerError {
+    SmeSuccess = 0,
+    SmeBundleIdInvalid,
+    SmeBundleNotFound,
+    SmeKeyNotFound,
+    SmeValueInvalid,
+    SmeKeyInvalid,
+    // Generic errors.
+    SmeReadFailed,
+    SmeWriteFailed,
+    SmeDeleteFailed,
+}
+
+#[inline]
+#[allow(dead_code)]
+pub fn kata_storage_delete(key: &str) -> Result<(), StorageManagerError> {
+    // NB: this assumes the StorageManager component is named "storage".
+    extern "C" {
+        pub fn storage_delete(
+            c_key: *const cstr_core::c_char
+        ) -> StorageManagerError;
+    }
+    let cstr = CString::new(key)
+        .map_err(|_| StorageManagerError::SmeKeyInvalid)?;
+    match unsafe { storage_delete(cstr.as_ptr()) } {
+        StorageManagerError::SmeSuccess => Ok(()),
+        status => Err(status),
+    }
+}
+
+#[inline]
+#[allow(dead_code)]
+pub fn kata_storage_read(key: &str) -> Result<KeyValueData, StorageManagerError> {
+    extern "C" {
+        fn storage_read(
+            c_key: *const cstr_core::c_char,
+            c_raw_value: *mut KeyValueData,
+        ) -> StorageManagerError;
+    }
+    let cstr = CString::new(key)
+        .map_err(|_| StorageManagerError::SmeKeyInvalid)?;
+    let value = &mut [0u8; KEY_VALUE_DATA_SIZE];
+    match unsafe { storage_read(cstr.as_ptr(), value as *mut _) } {
+        StorageManagerError::SmeSuccess => Ok(*value),
+        status => Err(status),
+    }
+}
+
+#[inline]
+#[allow(dead_code)]
+pub fn kata_storage_write(key: &str, value: &[u8]) -> Result<(), StorageManagerError> {
+    extern "C" {
+        fn storage_write(
+            c_key: *const cstr_core::c_char,
+            c_raw_value_len: usize,
+            c_raw_value: *const u8,
+        ) -> StorageManagerError;
+    }
+    let cstr = CString::new(key)
+        .map_err(|_| StorageManagerError::SmeKeyInvalid)?;
+    match unsafe { storage_write(cstr.as_ptr(), value.len(), value.as_ptr()) } {
+        StorageManagerError::SmeSuccess => Ok(()),
+        status => Err(status),
+    }
+}

AmbiML/sparrow-cantrip-full

apps/system/components/ProcessManager/kata-proc-manager/src/lib.rs
@@ -4,6 +4,7 @@
 
 extern crate alloc;
 use alloc::string::String;
+use core::slice;
 use cantrip_proc_common::*;
 use cantrip_security_common::*;
 use log::trace;
@@ -89,14 +90,11 @@ impl ProcessManagerInterface for CantripManagerInterface {
 
         // This is handled by the SecurityCoordinator.
         let reply = &mut [0u8; SECURITY_REPLY_DATA_SIZE];
-        match unsafe {
-            security_request(
-                SecurityRequest::SrInstall,
-                pkg_buffer_size,
-                pkg_buffer,
-                reply as *mut _,
-            )
-        } {
+        match cantrip_security_request(
+            SecurityRequest::SrInstall,
+            unsafe { slice::from_raw_parts(pkg_buffer, pkg_buffer_size as usize) },
+            reply,
+        ) {
             SecurityRequestError::SreSuccess => {
                 fn deserialize_failure(e: postcard::Error) -> ProcessManagerError {
                     trace!("install failed: deserialize {:?}", e);
@@ -123,14 +121,7 @@ impl ProcessManagerInterface for CantripManagerInterface {
         let mut request_data = [0u8; SECURITY_REQUEST_DATA_SIZE];
         let _ = postcard::to_slice(&bundle_id, &mut request_data).map_err(serialize_failure)?;
         let reply = &mut [0u8; SECURITY_REPLY_DATA_SIZE];
-        match unsafe {
-            security_request(
-                SecurityRequest::SrUninstall,
-                request_data.len() as u32,
-                request_data.as_ptr(),
-                reply as *mut _,
-            )
-        } {
+        match cantrip_security_request(SecurityRequest::SrUninstall, &request_data, reply) {
             SecurityRequestError::SreSuccess => Ok(()),
             status => {
                 trace!("uninstall failed: {:?}", status);
@@ -139,7 +130,7 @@ impl ProcessManagerInterface for CantripManagerInterface {
         }
     }
     fn start(&mut self, _bundle: &Bundle) -> Result<(), ProcessManagerError> {
-        // 1. Ask security core for application footprint with SizeBuffer 
+        // 1. Ask security core for application footprint with SizeBuffer
         // 2. Ask security core for manifest (maybe piggyback on SizeBuffer)
         //    and parse for necessary info (e.g. whether kv Storage is
         //    required, other privileges/capabilities)

apps/system/components/SecurityCoordinator/kata-security-common/src/lib.rs
@@ -45,6 +45,8 @@ mod mut_ptr_helper {
     }
 }
 
+// TODO(sleffler): convert String to &str
+
 // NB: SecurityRequestInstall is handled specially.
 
 // SecurityRequestUninstall
@@ -145,8 +147,8 @@ pub enum SecurityRequest {
     SrInstall,   // Install package [pkg_buffer] -> bundle_id
     SrUninstall, // Uninstall package [bundle_id]
 
-    SrSizeBuffer, // Size application image [bundle_id] -> u32
-    SrGetManifest, // Return application manifest [bundle_id] -> String
+    SrSizeBuffer,      // Size application image [bundle_id] -> u32
+    SrGetManifest,     // Return application manifest [bundle_id] -> String
     SrLoadApplication, // Load application [bundle_id]
     // TODO(sleffler): define <tag>?
     SrLoadModel, // Load ML model [bundle_id, <tag>]
@@ -166,14 +168,29 @@ pub trait SecurityCoordinatorInterface {
     ) -> Result<(), SecurityRequestError>;
 }
 
-// Camkes-generated rpc api.
-// NB: this requires the SecurityCoordinator component be named "security".
+// TODO(sleffler): try cantrip_security_request<T> to lower serde work
+#[inline]
 #[allow(dead_code)]
-extern "C" {
-    pub fn security_request(
-        c_request: SecurityRequest,
-        c_request_buffer_len: u32,
-        c_request_buffer: *const u8,
-        c_reply_buffer: *mut SecurityReplyData,
-    ) -> SecurityRequestError;
+pub fn cantrip_security_request(
+    request: SecurityRequest,
+    request_buffer: &[u8],
+    reply_buffer: &mut SecurityReplyData,
+) -> SecurityRequestError {
+    // NB: this assumes the SecurityCoordinator component is named "security".
+    extern "C" {
+        pub fn security_request(
+            c_request: SecurityRequest,
+            c_request_buffer_len: u32,
+            c_request_buffer: *const u8,
+            c_reply_buffer: *mut SecurityReplyData,
+        ) -> SecurityRequestError;
+    }
+    unsafe {
+        security_request(
+            request,
+            request_buffer.len() as u32,
+            request_buffer.as_ptr(),
+            reply_buffer as *mut _,
+        )
+    }
 }

apps/system/components/SecurityCoordinator/kata-security-coordinator/src/impl.rs
@@ -5,17 +5,17 @@ use cantrip_security_common::*;
 use log::trace;
 use postcard;
 
-pub struct SeL4SecurityCoordinatorInterface {
+pub struct SeL4SecurityCoordinator {
     // TODO(sleffler): mailbox ipc state
 }
-impl SeL4SecurityCoordinatorInterface {
+impl SeL4SecurityCoordinator {
     pub fn new() -> Self {
-        SeL4SecurityCoordinatorInterface {}
+        SeL4SecurityCoordinator {}
     }
 }
-pub type CantripSecurityCoordinatorInterface = SeL4SecurityCoordinatorInterface;
+pub type CantripSecurityCoordinatorInterface = SeL4SecurityCoordinator;
 
-impl SecurityCoordinatorInterface for SeL4SecurityCoordinatorInterface {
+impl SecurityCoordinatorInterface for SeL4SecurityCoordinator {
     fn request(
         &mut self,
         request_id: SecurityRequest,

apps/system/components/StorageManager/kata-storage-component/src/run.rs
@@ -1,0 +1,120 @@
+//! Cantrip OS StorageManager component support.
+
+// Code here binds the camkes component to the rust code.
+#![no_std]
+
+extern crate alloc;
+use core::slice;
+use cstr_core::CStr;
+extern crate cantrip_panic;
+use cantrip_allocator;
+use cantrip_logger::CantripLogger;
+use cantrip_storage_interface::KeyValueData;
+use cantrip_storage_interface::StorageError;
+use cantrip_storage_interface::StorageManagerInterface;
+use cantrip_storage_interface::StorageManagerError;
+use cantrip_storage_manager::CANTRIP_STORAGE;
+use log::trace;
+
+#[no_mangle]
+pub extern "C" fn pre_init() {
+    static CANTRIP_LOGGER: CantripLogger = CantripLogger;
+    log::set_logger(&CANTRIP_LOGGER).unwrap();
+    // NB: set to max; the LoggerInterface will filter
+    log::set_max_level(log::LevelFilter::Trace);
+
+    // TODO(sleffler): temp until we integrate with seL4
+    static mut HEAP_MEMORY: [u8; 8 * 1024] = [0; 8 * 1024];
+    unsafe {
+        cantrip_allocator::ALLOCATOR.init(HEAP_MEMORY.as_mut_ptr() as usize, HEAP_MEMORY.len());
+        trace!(
+            "setup heap: start_addr {:p} size {}",
+            HEAP_MEMORY.as_ptr(),
+            HEAP_MEMORY.len()
+        );
+    }
+}
+
+fn map_storage_error(se: StorageError, def: StorageManagerError) -> StorageManagerError {
+    match se {
+        StorageError::Success => StorageManagerError::SmeSuccess,
+        StorageError::BundleNotFound => StorageManagerError::SmeBundleNotFound,
+        StorageError::KeyNotFound => StorageManagerError::SmeKeyNotFound,
+        StorageError::KeyInvalid => StorageManagerError::SmeKeyInvalid,
+        StorageError::ValueInvalid => StorageManagerError::SmeValueInvalid,
+        _ => def,
+    }
+}
+
+// StorageInterface glue stubs.
+#[no_mangle]
+pub extern "C" fn storage_read(
+    c_key: *const cstr_core::c_char,
+    c_raw_value: *mut KeyValueData,
+) -> StorageManagerError {
+    unsafe {
+        match CStr::from_ptr(c_key).to_str() {
+            Ok(key) => {
+                // TODO(sleffler): de-badge reply cap to get bundle_id
+                match CANTRIP_STORAGE.read("fubar", key) {
+                    // NB: no serialization, returns raw data
+                    Ok(value) => {
+                        (*c_raw_value).copy_from_slice(&value);
+                        StorageManagerError::SmeSuccess
+                    }
+                    Err(e) => map_storage_error(e, StorageManagerError::SmeReadFailed),
+                }
+            }
+            Err(e) => {
+                trace!("read: keyinvalid {:?}", e);
+                StorageManagerError::SmeKeyInvalid
+            }
+        }
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn storage_write(
+    c_key: *const cstr_core::c_char,
+    c_raw_value_len: usize,
+    c_raw_value: *const u8,
+) -> StorageManagerError {
+    unsafe {
+        match CStr::from_ptr(c_key).to_str() {
+            Ok(key) => {
+                // TODO(sleffler): de-badge reply cap to get bundle_id
+                match CANTRIP_STORAGE.write(
+                    "fubar",
+                    key,
+                    slice::from_raw_parts(c_raw_value, c_raw_value_len),
+                ) {
+                    Ok(_) => StorageManagerError::SmeSuccess,
+                    Err(e) => map_storage_error(e, StorageManagerError::SmeWriteFailed),
+                }
+            }
+            Err(e) => {
+                trace!("write: keyinvalid {:?}", e);
+                StorageManagerError::SmeKeyInvalid
+            }
+        }
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn storage_delete(c_key: *const cstr_core::c_char) -> StorageManagerError {
+    unsafe {
+        match CStr::from_ptr(c_key).to_str() {
+            Ok(key) => {
+                // TODO(sleffler): de-badge reply cap to get bundle_id
+                match CANTRIP_STORAGE.delete("fubar", key) {
+                    Ok(_) => StorageManagerError::SmeSuccess,
+                    Err(e) => map_storage_error(e, StorageManagerError::SmeDeleteFailed),
+                }
+            }
+            Err(e) => {
+                trace!("delete: keyinvalid {:?}", e);
+                StorageManagerError::SmeKeyInvalid
+            }
+        }
+    }
+}

apps/system/components/StorageManager/kata-storage-interface/src/lib.rs
@@ -1,0 +1,105 @@
+//! Cantrip OS storage management support
+
+#![cfg_attr(not(test), no_std)]
+
+use core::str;
+use cstr_core::CString;
+
+// TODO(sleffler): temp constraint on value part of key-value pairs
+pub const KEY_VALUE_DATA_SIZE: usize = 100;
+pub type KeyValueData = [u8; KEY_VALUE_DATA_SIZE];
+
+// NB: struct's marked repr(C) are processed by cbindgen to get a .h file
+//   used in camkes C interfaces.
+
+#[derive(Debug, Eq, PartialEq)]
+pub enum StorageError {
+    Success = 0,
+    BundleNotFound,
+    KeyNotFound,
+    KeyInvalid,
+    ValueInvalid,
+    SerializeFailed,
+    // Generic errors.
+    ReadFailed,
+    WriteFailed,
+    DeleteFailed,
+}
+
+pub trait StorageManagerInterface {
+    fn read(&self, bundle_id: &str, key: &str) -> Result<KeyValueData, StorageError>;
+    fn write(&self, bundle_id: &str, key: &str, value: &[u8]) -> Result<(), StorageError>;
+    fn delete(&self, bundle_id: &str, key: &str) -> Result<(), StorageError>;
+}
+
+// Public version of StorageError presented over rpc interface.
+// This is needed because the enum is exported to C users and needs to
+// be unique from other enum's.
+// TODO(sleffler): switch to single generic error space ala absl::StatusCode
+#[repr(C)]
+#[derive(Debug, Eq, PartialEq)]
+pub enum StorageManagerError {
+    SmeSuccess = 0,
+    SmeBundleIdInvalid,
+    SmeBundleNotFound,
+    SmeKeyNotFound,
+    SmeValueInvalid,
+    SmeKeyInvalid,
+    // Generic errors.
+    SmeReadFailed,
+    SmeWriteFailed,
+    SmeDeleteFailed,
+}
+
+#[inline]
+#[allow(dead_code)]
+pub fn cantrip_storage_delete(key: &str) -> Result<(), StorageManagerError> {
+    // NB: this assumes the StorageManager component is named "storage".
+    extern "C" {
+        pub fn storage_delete(
+            c_key: *const cstr_core::c_char
+        ) -> StorageManagerError;
+    }
+    let cstr = CString::new(key)
+        .map_err(|_| StorageManagerError::SmeKeyInvalid)?;
+    match unsafe { storage_delete(cstr.as_ptr()) } {
+        StorageManagerError::SmeSuccess => Ok(()),
+        status => Err(status),
+    }
+}
+
+#[inline]
+#[allow(dead_code)]
+pub fn cantrip_storage_read(key: &str) -> Result<KeyValueData, StorageManagerError> {
+    extern "C" {
+        fn storage_read(
+            c_key: *const cstr_core::c_char,
+            c_raw_value: *mut KeyValueData,
+        ) -> StorageManagerError;
+    }
+    let cstr = CString::new(key)
+        .map_err(|_| StorageManagerError::SmeKeyInvalid)?;
+    let value = &mut [0u8; KEY_VALUE_DATA_SIZE];
+    match unsafe { storage_read(cstr.as_ptr(), value as *mut _) } {
+        StorageManagerError::SmeSuccess => Ok(*value),
+        status => Err(status),
+    }
+}
+
+#[inline]
+#[allow(dead_code)]
+pub fn cantrip_storage_write(key: &str, value: &[u8]) -> Result<(), StorageManagerError> {
+    extern "C" {
+        fn storage_write(
+            c_key: *const cstr_core::c_char,
+            c_raw_value_len: usize,
+            c_raw_value: *const u8,
+        ) -> StorageManagerError;
+    }
+    let cstr = CString::new(key)
+        .map_err(|_| StorageManagerError::SmeKeyInvalid)?;
+    match unsafe { storage_write(cstr.as_ptr(), value.len(), value.as_ptr()) } {
+        StorageManagerError::SmeSuccess => Ok(()),
+        status => Err(status),
+    }
+}