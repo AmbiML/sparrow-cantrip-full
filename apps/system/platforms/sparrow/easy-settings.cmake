a69d23de1c0486bea94e75ee31ebf7ff6da72bd0
Sam Leffler <sleffler@google.com>
2023-01-30 17:31:14 +0000

    Merge "cantrip: trim rootserver configs on sparrow to reduce bloat"
    GitOrigin-RevId: 0858de805c6dda0a971a0fbeb8ed0cd2534a67b1

---
+++ apps/system/platforms/sparrow/easy-settings.cmake
@@ -0,0 +1,17 @@
+# Sparrow-specific configuration.
+
+if (RELEASE)
+  set(CAmkESFaultHandlers OFF CACHE BOOL "Debugging aid")
+endif()
+
+# Carefully size the rootserver data. Peak memory use during boot is when
+# the rootserver runs so we tune this and the rootserver's internal data
+# structure sizes to minimize waste.
+if (RELEASE)
+  set(KernelRootCNodeSizeBits 11 CACHE STRING "Root CNode Size (2^n slots)")
+  set(KernelMaxNumBootinfoUntypedCaps 128 CACHE STRING "Max number of bootinfo untyped caps")
+else()
+  # NB: for Sparrow, 13 works but is tight
+  set(KernelRootCNodeSizeBits 13 CACHE STRING "Root CNode Size (2^n slots)")
+  set(KernelMaxNumBootinfoUntypedCaps 128 CACHE STRING "Max number of bootinfo untyped caps")
+endif()
