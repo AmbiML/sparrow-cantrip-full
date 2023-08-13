4c3a5185579e43ee576ba1ae7983bcf14f275a88
Sam Leffler <sleffler@google.com>
2023-08-13 15:12:01 -0700

    camkes:semaphore: fill-in a real implementation
    This replaces the nonsense impl of seL4_Semaphore with a "real" impl
    built on top of seL4_BareSema. The initial value is fixed at "1" unless
    you create one directly (CAmkES lets you specify the initial value but
    our template does not plumb it).
    
    Change-Id: I4a5bc2efea3eaf1b6079aeda6d5ec3e84d01983d
    GitOrigin-RevId: baf528a835b5de7c592833bf016d761eca993bae

--- apps/system/components/cantrip-os-common/src/camkes/src/semaphore.rs
+++ apps/system/components/cantrip-os-common/src/camkes/src/semaphore.rs
@@ -12,11 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use core::ptr;
-
+use crate::baresema::seL4_BareSema;
 use sel4_sys::seL4_CPtr;
-use sel4_sys::seL4_Signal;
-use sel4_sys::seL4_Wait;
 
 #[macro_export]
 macro_rules! static_semaphore {
@@ -26,38 +23,29 @@ macro_rules! static_semaphore {
     ($sem_tag:ident, $sem_name:expr) => {
         crate::paste! {
             pub static [<$sem_tag: upper>]: seL4_Semaphore =
-                seL4_Semaphore::new($sem_name, [<$sem_tag:upper _ENDPOINT>]);
+                seL4_Semaphore::new($sem_name, [<$sem_tag:upper _ENDPOINT>], 1);
         }
     };
 }
 
 #[derive(Debug)]
 pub struct seL4_Semaphore {
-    name: &'static str,
-    endpoint: seL4_CPtr, // Semaphore endpoint object
+    sem: seL4_BareSema,
 }
 impl seL4_Semaphore {
-    pub const fn new(name: &'static str, endpoint: seL4_CPtr) -> Self { Self { name, endpoint } }
-    pub fn name(&self) -> &str { self.name }
+    pub const fn new(name: &'static str, endpoint: seL4_CPtr, value: isize) -> Self {
+        Self {
+            sem: seL4_BareSema::new(name, endpoint, value),
+        }
+    }
+    pub fn name(&self) -> &str { self.sem.name() }
 
     /// Waits (non-blocking) for a semaphore.
-    pub fn try_wait(&self) {
-        unsafe {
-            seL4_Wait(self.endpoint, ptr::null_mut());
-        }
-    }
+    pub fn try_wait(&self) { self.sem.try_wait(); }
 
     /// Waits (blocking) for a semaphore.
-    pub fn wait(&self) {
-        unsafe {
-            seL4_Wait(self.endpoint, ptr::null_mut());
-        }
-    }
+    pub fn wait(&self) { self.sem.wait(); }
 
     /// Releases a semaphore, signaling waiters.
-    pub fn post(&self) {
-        unsafe {
-            seL4_Signal(self.endpoint);
-        }
-    }
+    pub fn post(&self) { self.sem.post(); }
 }
